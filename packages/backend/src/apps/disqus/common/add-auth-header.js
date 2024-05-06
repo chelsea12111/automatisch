131113d0d0674be1cfd4352f212a49bd59777e76
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:29 +0100

    improve

--- packages/backend/src/apps/disqus/common/add-auth-header.js
+++ packages/backend/src/apps/disqus/common/add-auth-header.js
@@ -1,10 +1,19 @@
 import { URLSearchParams } from 'url';
 
-const addAuthHeader = ($, requestConfig) => {
+const addAuthHeader = (axiosInstance, requestConfig) => {
+  if (!axiosInstance || !requestConfig) {
+    throw new Error('axiosInstance and requestConfig are required parameters');
+  }
+
+  const authData = axiosInstance.defaults.headers.common['Authorization'].data;
+  if (!authData) {
+    throw new Error('Authorization data not found in axiosInstance defaults');
+  }
+
   const params = new URLSearchParams({
-    access_token: $.auth.data.accessToken,
-    api_key: $.auth.data.apiKey,
-    api_secret: $.auth.data.apiSecret,
+    access_token: authData.accessToken,
+    api_key: authData.apiKey,
+    api_secret: authData.apiSecret,
   });
 
   requestConfig.params = params;

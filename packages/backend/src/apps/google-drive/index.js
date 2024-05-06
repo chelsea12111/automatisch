13c2ee6b8760b90aca1904a5e9730b52852fa83b
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:57 +0100

    improve

--- packages/backend/src/apps/google-drive/index.js
+++ packages/backend/src/apps/google-drive/index.js
@@ -1,10 +1,10 @@
 import defineApp from '../../helpers/define-app.js';
 import addAuthHeader from './common/add-auth-header.js';
-import auth from './auth/index.js';
-import triggers from './triggers/index.js';
-import dynamicData from './dynamic-data/index.js';
+import authConfig from './auth/index.js';
+import triggersConfig from './triggers/index.js';
+import dynamicDataConfig from './dynamic-data/index.js';
 
-export default defineApp({
+const appConfig = {
   name: 'Google Drive',
   key: 'google-drive',
   baseUrl: 'https://drive.google.com',
@@ -14,7 +14,10 @@ export default defineApp({
   primaryColor: '1FA463',
   supportsConnections: true,
   beforeRequest: [addAuthHeader],
-  auth,
-  triggers,
-  dynamicData,
-});
+};
+
+export const app = defineApp(appConfig);
+export const auth = authConfig;
+export const triggers = triggersConfig;
+export const dynamicData = dynamicDataConfig;
+

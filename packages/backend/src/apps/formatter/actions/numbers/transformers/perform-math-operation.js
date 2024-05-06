a16ffdd6d3de701134f176c40479b8408d43a730
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:38 +0100

    improve

--- packages/backend/src/apps/formatter/actions/numbers/transformers/perform-math-operation.js
+++ packages/backend/src/apps/formatter/actions/numbers/transformers/perform-math-operation.js
@@ -1,22 +1,25 @@
-import add from 'lodash/add.js';
-import divide from 'lodash/divide.js';
-import multiply from 'lodash/multiply.js';
-import subtract from 'lodash/subtract.js';
+import add from 'lodash/add';
+import divide from 'lodash/divide';
+import multiply from 'lodash/multiply';
+import subtract from 'lodash/subtract';
 
 const mathOperation = ($) => {
   const mathOperation = $.step.parameters.mathOperation;
   const values = $.step.parameters.values.map((value) => Number(value.input));
 
-  if (mathOperation === 'add') {
-    return values.reduce((acc, curr) => add(acc, curr), 0);
-  } else if (mathOperation === 'divide') {
-    return values.reduce((acc, curr) => divide(acc, curr));
-  } else if (mathOperation === 'makeNegative') {
-    return values.map((value) => -value);
-  } else if (mathOperation === 'multiply') {
-    return values.reduce((acc, curr) => multiply(acc, curr), 1);
-  } else if (mathOperation === 'subtract') {
-    return values.reduce((acc, curr) => subtract(acc, curr));
+  switch (mathOperation) {
+    case 'add':
+      return values.reduce((acc, curr) => add(acc, curr), 0);
+    case 'divide':
+      return values.reduce((acc, curr) => divide(acc, curr));
+    case 'makeNegative':
+      return values.map((value) => -value);
+    case 'multiply':
+      return values.reduce((acc, curr) => multiply(acc, curr), 1);
+    case 'subtract':
+      return values.reduce((acc, curr) => subtract(acc, curr));
+    default:
+      throw new Error(`Invalid math operation: ${mathOperation}`);
   }
 };
 

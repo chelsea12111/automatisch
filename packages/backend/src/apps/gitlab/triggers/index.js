bd39b63da11142763b9deed0e6ff9cad0b3eabd0
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:41 +0100

    improve

--- packages/backend/src/apps/gitlab/triggers/index.js
+++ packages/backend/src/apps/gitlab/triggers/index.js
@@ -1,29 +1,22 @@
-import confidentialIssueEvent from './confidential-issue-event/index.js';
-import confidentialNoteEvent from './confidential-note-event/index.js';
-import deploymentEvent from './deployment-event/index.js';
-import featureFlagEvent from './feature-flag-event/index.js';
-import issueEvent from './issue-event/index.js';
-import jobEvent from './job-event/index.js';
-import mergeRequestEvent from './merge-request-event/index.js';
-import noteEvent from './note-event/index.js';
-import pipelineEvent from './pipeline-event/index.js';
-import pushEvent from './push-event/index.js';
-import releaseEvent from './release-event/index.js';
-import tagPushEvent from './tag-push-event/index.js';
-import wikiPageEvent from './wiki-page-event/index.js';
-
-export default [
-  confidentialIssueEvent,
-  confidentialNoteEvent,
-  deploymentEvent,
-  featureFlagEvent,
-  issueEvent,
-  jobEvent,
-  mergeRequestEvent,
-  noteEvent,
-  pipelineEvent,
-  pushEvent,
-  releaseEvent,
-  tagPushEvent,
-  wikiPageEvent,
+const eventModules = [
+  ['confidential-issue-event', 'index.js'],
+  ['confidential-note-event', 'index.js'],
+  ['deployment-event', 'index.js'],
+  ['feature-flag-event', 'index.js'],
+  ['issue-event', 'index.js'],
+  ['job-event', 'index.js'],
+  ['merge-request-event', 'index.js'],
+  ['note-event', 'index.js'],
+  ['pipeline-event', 'index.js'],
+  ['push-event', 'index.js'],
+  ['release-event', 'index.js'],
+  ['tag-push-event', 'index.js'],
+  ['wiki-page-event', 'index.js'],
 ];
+
+const importModule = ([moduleName, filePath]) =>
+  import(`./${moduleName}/${filePath}`).then((module) => module.default);
+
+const loadEventModules = () => Promise.all(eventModules.map(importModule));
+
+export default loadEventModules;

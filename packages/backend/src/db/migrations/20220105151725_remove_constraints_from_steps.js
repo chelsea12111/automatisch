44bfbabf7914fa15b0c1788e1dada7f4f2a7b594
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:58 +0100

    improve

--- packages/backend/src/db/migrations/20220105151725_remove_constraints_from_steps.js
+++ packages/backend/src/db/migrations/20220105151725_remove_constraints_from_steps.js
@@ -1,12 +1,12 @@
 export async function up(knex) {
-  return knex.schema.alterTable('steps', (table) => {
-    table.string('key').nullable().alter();
-    table.string('app_key').nullable().alter();
+  return knex.schema.table('steps', (table) => {
+    table.string('key').defaultTo(null).alter();
+    table.string('app_key').defaultTo(null).alter();
   });
 }
 
 export async function down(knex) {
-  return knex.schema.alterTable('steps', (table) => {
+  return knex.schema.table('steps', (table) => {
     table.string('key').notNullable().alter();
     table.string('app_key').notNullable().alter();
   });

d8a311a12986d88ad8ab326ec66c3c846ed0e06e
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:17 +0100

    improve

--- packages/backend/src/apps/reddit/common/auth-scope.js
+++ packages/backend/src/apps/reddit/common/auth-scope.js
@@ -1,10 +1,6 @@
 // authScope.js
 
-const authScope = ['identity', 'read', 'account', 'submit'];
-
-module.exports = {
-  authScope,
-};
+export const authScope = ['identity', 'read', 'account', 'submit'];
 
 
 // someOtherModule.js
@@ -14,3 +10,8 @@ import { authScope } from './authScope.js';
 // Use authScope array here
 console.log(authScope); // ['identity', 'read', 'account', 'submit']
 
+// Additional code to demonstrate using the authScope array
+authScope.forEach(permission => {
+  console.log(`Checking permission: ${permission}`);
+  // Perform permission checks or other logic here
+});

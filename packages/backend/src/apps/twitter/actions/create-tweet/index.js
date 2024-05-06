272aa208e26284784b4d50c3d17f2a3918a78d20
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:05 +0100

    improve

--- packages/backend/src/apps/twitter/actions/create-tweet/index.js
+++ packages/backend/src/apps/twitter/actions/create-tweet/index.js
@@ -1,9 +1,10 @@
 import defineAction from '../../../../helpers/define-action.js';
+import { tweetApiBaseUrl, tweetApiVersion } from '../../../../config.js';
 
 export default defineAction({
   name: 'Create tweet',
   key: 'createTweet',
-  description: 'Create a tweet.',
+  description: 'Create a new tweet.',
   arguments: [
     {
       label: 'Tweet body',
@@ -16,10 +17,19 @@ export default defineAction({
   ],
 
   async run($) {
-    const text = $.step.parameters.tweet;
-    const response = await $.http.post('/2/tweets', {
-      text,
+    const tweetContent = $.step.parameters.tweet;
+
+    if (!tweetContent.trim()) {
+      throw new Error('Tweet body cannot be empty.');
+    }
+
+    const response = await $.http.post(`${tweetApiBaseUrl}/${tweetApiVersion}/tweets`, {
+      text: tweetContent,
     });
+
+    if (!response.ok) {
+      throw new Error(`Failed to create tweet: ${response.statusText}`);
+    }
 
     $.setActionItem({ raw: response.data });
   },

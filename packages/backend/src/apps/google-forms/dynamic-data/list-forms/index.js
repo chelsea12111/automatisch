16cfcac9b9f4e80b8f778840520c9bdb870ce74e
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:51 +0100

    improve

--- packages/backend/src/apps/google-forms/dynamic-data/list-forms/index.js
+++ packages/backend/src/apps/google-forms/dynamic-data/list-forms/index.js
@@ -14,20 +14,28 @@ export default {
     };
 
     do {
-      const { data } = await $.http.get(
+      const response = await $.http.get(
         `https://www.googleapis.com/drive/v3/files`,
         { params }
       );
+
+      const { data } = response;
+      if (!data || !data.files) {
+        break;
+      }
+
       params.pageToken = data.nextPageToken;
 
-      for (const file of data.files) {
-        forms.data.push({
+      forms.data.push(
+        ...data.files.map(file => ({
           value: file.id,
           name: file.name,
-        });
-      }
+        }))
+      );
+
     } while (params.pageToken);
 
     return forms;
   },
 };
+

8d8b3332d41b2069b41e9ec9f75428d87e172994
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:50 +0100

    improve

--- packages/backend/src/helpers/graphql-instance.js
+++ packages/backend/src/helpers/graphql-instance.js
@@ -1,10 +1,11 @@
-import path, { join } from 'path';
+import path from 'path';
 import { fileURLToPath } from 'url';
 import { graphqlHTTP } from 'express-graphql';
-import { loadSchemaSync } from '@graphql-tools/load';
+import { loadSchemaSync, makeExecutableSchema } from '@graphql-tools/load';
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 import { addResolversToSchema } from '@graphql-tools/schema';
 import { applyMiddleware } from 'graphql-middleware';
+import { makeExecutable } from 'graphql-middleware';
 
 import appConfig from '../config/app.js';
 import logger from './logger.js';
@@ -13,24 +14,36 @@ import * as Sentry from './sentry.ee.js';
 import resolvers from '../graphql/resolvers.js';
 import HttpError from '../errors/http.js';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const schema = loadSchemaSync(join(__dirname, '../graphql/schema.graphql'), {
-  loaders: [new GraphQLFileLoader()],
-});
+let schema;
+try {
+  schema = loadSchemaSync(path.resolve(__dirname, '../graphql/schema.graphql'), {
+    loaders: [new GraphQLFileLoader()],
+  });
+} catch (e) {
+  console.error('Error loading schema:', e);
+  process.exit(1);
+}
 
 const schemaWithResolvers = addResolversToSchema({
   schema,
   resolvers,
 });
 
+const schemaWithMiddleware = applyMiddleware(
+  makeExecutableSchema({
+    schema: schemaWithResolvers,
+  }),
+  authentication.generate(schemaWithResolvers)
+);
+
 const graphQLInstance = graphqlHTTP({
-  schema: applyMiddleware(
-    schemaWithResolvers,
-    authentication.generate(schemaWithResolvers)
-  ),
+  schema: makeExecutable(schemaWithMiddleware),
+  rootValue: {},
   graphiql: appConfig.isDev,
-  customFormatErrorFn: (error) => {
+  customFormatErrorFn: async (error) => {
     logger.error(error.path + ' : ' + error.message + '\n' + error.stack);
 
     if (error.originalError instanceof HttpError) {

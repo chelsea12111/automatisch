d9ffb9ead88d763e1d548c8dc8cc7c8a840a787f
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:18 +0100

    improve

--- packages/backend/src/apps/reddit/common/auth-scope.js
+++ packages/backend/src/apps/reddit/common/auth-scope.js
@@ -1,3 +1,16 @@
+// authScope.js
+
 const authScope = ['identity', 'read', 'account', 'submit'];
 
-export default authScope;
+module.exports = {
+  authScope,
+};
+
+
+// someOtherModule.js
+
+import { authScope } from './authScope.js';
+
+// Use authScope array here
+console.log(authScope); // ['identity', 'read', 'account', 'submit']
+

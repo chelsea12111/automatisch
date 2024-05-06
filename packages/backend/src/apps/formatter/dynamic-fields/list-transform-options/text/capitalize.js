61a03d312591857fa3316145f5d82e2e41343102
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:36 +0100

    improve

--- packages/backend/src/apps/formatter/dynamic-fields/list-transform-options/text/capitalize.js
+++ packages/backend/src/apps/formatter/dynamic-fields/list-transform-options/text/capitalize.js
@@ -1,12 +1,23 @@
-const capitalize = [
-  {
-    label: 'Input',
-    key: 'input',
-    type: 'string',
-    required: true,
-    description: 'Text that will be capitalized.',
-    variables: true,
-  },
-];
+interface CapitalizeSchema {
+  label: string;
+  key: 'input';
+  type: 'string';
+  required: true;
+  description: string;
+  variables: boolean;
+  pattern?: string;
+  minLength?: number;
+}
+
+const capitalize: CapitalizeSchema = {
+  label: 'Input',
+  key: 'input',
+  type: 'string',
+  required: true,
+  description: 'Text that will be capitalized.',
+  variables: true,
+  pattern: '^[a-zA-Z0-9\\s]+$',
+  minLength: 1
+};
 
 export default capitalize;

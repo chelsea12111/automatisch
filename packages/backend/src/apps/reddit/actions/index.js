c4479cfe39bd5ec5b5dd2b4508a9b4aad6724334
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:31 +0100

    improve

--- packages/backend/src/apps/reddit/actions/index.js
+++ packages/backend/src/apps/reddit/actions/index.js
@@ -1,3 +1,8 @@
-import createLinkPost from './create-link-post/index.js';
+// import the createLinkPost function from its module
+import createLinkPost from './create-link-post.js';
 
+// export an array containing the createLinkPost function
 export default [createLinkPost];
+
+// make sure to omit the .js extension when importing/exporting modules
+// this is a best practice that helps keep your code clean and consistent

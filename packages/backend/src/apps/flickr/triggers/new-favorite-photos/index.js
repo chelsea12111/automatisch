c7774886ee15d8d7c297a05aebe8fb47974f38ac
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:12 +0100

    improve

--- packages/backend/src/apps/flickr/triggers/new-favorite-photos/index.js
+++ packages/backend/src/apps/flickr/triggers/new-favorite-photos/index.js
@@ -1,11 +1,16 @@
-import defineTrigger from '../../../../helpers/define-trigger.js';
+import { defineTrigger } from '../../../../helpers/index.js';
 import newFavoritePhotos from './new-favorite-photos.js';
 
+const triggerName = 'New favorite photos';
+const pollInterval = 15; // minutes
+const triggerKey = 'newFavoritePhotos';
+const triggerDescription = 'Triggers when you favorite a photo.';
+
 export default defineTrigger({
-  name: 'New favorite photos',
-  pollInterval: 15,
-  key: 'newFavoritePhotos',
-  description: 'Triggers when you favorite a photo.',
+  name: triggerName,
+  pollInterval,
+  key: triggerKey,
+  description: triggerDescription,
 
   async run($) {
     await newFavoritePhotos($);

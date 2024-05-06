49dbbb45a67c9725e73c9215baefd4d53318eb3a
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:31 +0100

    improve

--- packages/backend/src/apps/disqus/auth/index.js
+++ packages/backend/src/apps/disqus/auth/index.js
@@ -3,46 +3,43 @@ import verifyCredentials from './verify-credentials.js';
 import refreshToken from './refresh-token.js';
 import isStillVerified from './is-still-verified.js';
 
-export default {
-  fields: [
-    {
-      key: 'oAuthRedirectUrl',
-      label: 'OAuth Redirect URL',
-      type: 'string',
-      required: true,
-      readOnly: true,
-      value: '{WEB_APP_URL}/app/disqus/connections/add',
-      placeholder: null,
-      description:
-        'When asked to input a redirect URL in Disqus, enter the URL above.',
-      clickToCopy: true,
-    },
-    {
-      key: 'apiKey',
-      label: 'API Key',
-      type: 'string',
-      required: true,
-      readOnly: false,
-      value: null,
-      placeholder: null,
-      description: null,
-      clickToCopy: false,
-    },
-    {
-      key: 'apiSecret',
-      label: 'API Secret',
-      type: 'string',
-      required: true,
-      readOnly: false,
-      value: null,
-      placeholder: null,
-      description: null,
-      clickToCopy: false,
-    },
-  ],
+const WEB_APP_URL = '{WEB_APP_URL}';
 
-  generateAuthUrl,
-  verifyCredentials,
-  isStillVerified,
-  refreshToken,
-};
+const fields = [
+  {
+    key: 'oAuthRedirectUrl',
+    label: 'OAuth Redirect URL',
+    type: 'string',
+    required: true,
+    readOnly: true,
+    value: `${WEB_APP_URL}/app/disqus/connections/add`,
+    placeholder: null,
+    description:
+      'When asked to input a redirect URL in Disqus, enter the URL above.',
+    clickToCopy: true,
+  },
+  {
+    key: 'apiKey',
+    label: 'API Key',
+    type: 'string',
+    required: true,
+    readOnly: false,
+    value: null,
+    placeholder: null,
+    description: null,
+    clickToCopy: false,
+  },
+  {
+    key: 'apiSecret',
+    label: 'API Secret',
+    type: 'string',
+    required: true,
+    readOnly: false,
+    value: null,
+    placeholder: null,
+    description: null,
+    clickToCopy: false,
+  },
+];
+
+export { generateAuthUrl, verifyCredentials, isStillVerified, refreshToken, fields };

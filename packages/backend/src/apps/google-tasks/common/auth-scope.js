b08569726ed5ebedf829e0d67c2b535493a63f75
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:55 +0100

    improve

--- packages/backend/src/apps/google-tasks/common/auth-scope.js
+++ packages/backend/src/apps/google-tasks/common/auth-scope.js
@@ -1,7 +1,9 @@
-const authScope = [
+/** Google API authentication scopes. */
+const googleAuthScopes = [
   'https://www.googleapis.com/auth/tasks',
   'https://www.googleapis.com/auth/userinfo.email',
   'https://www.googleapis.com/auth/userinfo.profile',
-];
+] as const;
 
-export default authScope;
+/** The exported Google API authentication scopes. */
+export default googleAuthScopes;

92a1ad3ff4159a58d690cb92619fe138425d22bc
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:50 +0100

    improve

--- packages/backend/src/apps/twilio/dynamic-data/index.js
+++ packages/backend/src/apps/twilio/dynamic-data/index.js
@@ -1,3 +1,10 @@
-import listIncomingPhoneNumbers from './list-incoming-phone-numbers/index.js';
+// list-incoming-phone-numbers.js
+export function listIncomingPhoneNumbers() {
+  // implementation here
+}
+
+// main.js
+import { listIncomingPhoneNumbers } from './list-incoming-phone-numbers/index.js';
 
 export default [listIncomingPhoneNumbers];
+

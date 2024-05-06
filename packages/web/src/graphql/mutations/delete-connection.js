0bc02dd3293e73f5288eeb780482f0633975765e
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:51 +0100

    improve

--- packages/web/src/graphql/mutations/delete-connection.js
+++ packages/web/src/graphql/mutations/delete-connection.js
@@ -1,6 +1,21 @@
 import { gql } from '@apollo/client';
+
+// Add type definitions for the input
+export interface DeleteConnectionInput {
+  id: string;
+  // Add any other fields needed for the input
+}
+
+// Add a default description for the deleteConnection mutation
+const DELETE_CONNECTION_DESCRIPTION = "Deletes a connection given its id";
+
 export const DELETE_CONNECTION = gql`
-  mutation DeleteConnection($input: DeleteConnectionInput) {
-    deleteConnection(input: $input)
+  mutation DeleteConnection($input: DeleteConnectionInput!) {
+    deleteConnection(input: $input) {
+      id
+      // Add any other fields needed for the response
+    }
   }
-`;
+`, {
+  description: DELETE_CONNECTION_DESCRIPTION
+}

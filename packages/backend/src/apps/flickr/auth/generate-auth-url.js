c69998ce9e381fb803bdcbd7c11b35a3f2e66a3a
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:46 +0100

    improve

--- packages/backend/src/apps/flickr/auth/generate-auth-url.js
+++ packages/backend/src/apps/flickr/auth/generate-auth-url.js
@@ -2,15 +2,20 @@ import { URLSearchParams } from 'url';
 
 export default async function generateAuthUrl($) {
   const oauthRedirectUrlField = $.app.auth.fields.find(
-    (field) => field.key == 'oAuthRedirectUrl'
+    (field) => field.key === 'oAuthRedirectUrl'
   );
+
+  if (!oauthRedirectUrlField) {
+    throw new Error('Missing oAuthRedirectUrl field');
+  }
 
   const callbackUrl = oauthRedirectUrlField.value;
   const requestPath = '/oauth/request_token';
-  const data = { oauth_callback: callbackUrl };
+  const data = new URLSearchParams();
+  data.append('oauth.callback', callbackUrl);
 
   const response = await $.http.post(requestPath, data);
-  const responseData = Object.fromEntries(new URLSearchParams(response.data));
+  const responseData = Object.fromEntries(response.data.split('&').map(param => param.split('=')));
 
   await $.auth.set({
     url: `${$.app.apiBaseUrl}/oauth/authorize?oauth_token=${responseData.oauth_token}&perms=delete`,

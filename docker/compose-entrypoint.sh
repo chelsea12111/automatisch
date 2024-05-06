f04ed5e31f79846d8a8ccd6dc08151995576a9f8
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:18 +0100

    improve

--- docker/compose-entrypoint.sh
+++ docker/compose-entrypoint.sh
@@ -1,26 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-if [ ! -f /automatisch/storage/.env ]; then
-  >&2 echo "Saving environment variables"
-  ENCRYPTION_KEY="${ENCRYPTION_KEY:-$(openssl rand -base64 36)}"
-  WEBHOOK_SECRET_KEY="${WEBHOOK_SECRET_KEY:-$(openssl rand -base64 36)}"
-  APP_SECRET_KEY="${APP_SECRET_KEY:-$(openssl rand -base64 36)}"
-  echo "ENCRYPTION_KEY=$ENCRYPTION_KEY" >> /automatisch/storage/.env
-  echo "WEBHOOK_SECRET_KEY=$WEBHOOK_SECRET_KEY" >> /automatisch/storage/.env
-  echo "APP_SECRET_KEY=$APP_SECRET_KEY" >> /automatisch/storage/.env
+# Check if openssl is installed
+if ! command -v openssl &> /dev/null
+then
+    >&2 echo "Error: openssl is not installed."
+    exit 1
 fi
 
-# initiate env. vars. from /automatisch/storage/.env file
-export $(grep -v '^#' /automatisch/storage/.env | xargs)
+init_env_vars() {
+    if [ ! -f /automatisch/storage/.env ]; then
+        >&2 echo "Saving environment variables"
+        ENCRYPTION_KEY="$(openssl rand -base64 36)"
+        WEBHOOK_SECRET_KEY="$(openssl rand -base64 36)"
+        APP_SECRET_KEY="$(openssl rand -base64 36)"
+        echo "ENCRYPTION_KEY=$ENCRYPTION_KEY" >> /automatisch/storage/.env
+        echo "WEBHOOK_SECRET_KEY=$WEBHOOK_SECRET_KEY" >> /automatisch/storage/.env
+        echo "APP_SECRET_KEY=$APP_SECRET_KEY" >> /automatisch/storage/.env
+    fi
 
-# migration for webhook secret key, will be removed in the future.
-if [[ -z "${WEBHOOK_SECRET_KEY}" ]]; then
-  WEBHOOK_SECRET_KEY="$(openssl rand -base64 36)"
-  echo "WEBHOOK_SECRET_KEY=$WEBHOOK_SECRET_KEY" >> /automatisch/storage/.env
-fi
+    # initiate env. vars. from /automatisch/storage/.env file
+    export $(grep -v '^#' /automatisch/storage/.env | xargs)
 
+    # migration for webhook secret key, will be removed in the future.
+    if [[ -z "${WEBHOOK_SECRET_KEY}" ]]; then
+        WEBHOOK_SECRET_KEY="$(openssl rand -base64 36)"
+        echo "WEBHOOK_SECRET_KEY=$WEBHOOK_SECRET_KEY" >> /automatisch/storage/.env
+    fi
+}
+
+init_env_vars
 echo "Environment variables have been set!"
 
 sh /entrypoint.sh

d801ff4c364e9991fffbb20ab9b80ed464eb0e17
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:32 +0100

    improve

--- packages/backend/src/apps/flickr/triggers/index.js
+++ packages/backend/src/apps/flickr/triggers/index.js
@@ -1,6 +1,13 @@
-import newAlbums from './new-albums/index.js';
-import newFavoritePhotos from './new-favorite-photos/index.js';
-import newPhotos from './new-photos/index.js';
-import newPhotosInAlbums from './new-photos-in-album/index.js';
+// Import individual modules instead of default export
+import * as newAlbums from './new-albums/index.js';
+import * as newFavoritePhotos from './new-favorite-photos/index.js';
+import * as newPhotos from './new-photos/index.js';
+import * as newPhotosInAlbums from './new-photos-in-album/index.js';
 
-export default [newAlbums, newFavoritePhotos, newPhotos, newPhotosInAlbums];
+// Export an object with named properties for each module
+export default {
+  newAlbums,
+  newFavoritePhotos,
+  newPhotos,
+  newPhotosInAlbums,
+};

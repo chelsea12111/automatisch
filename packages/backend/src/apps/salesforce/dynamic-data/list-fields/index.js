55d800527e56748718ccba124f55d2f670916f9b
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:31 +0100

    improve

--- packages/backend/src/apps/salesforce/dynamic-data/list-fields/index.js
+++ packages/backend/src/apps/salesforce/dynamic-data/list-fields/index.js
@@ -1,23 +1,57 @@
-export default {
-  name: 'List fields',
-  key: 'listFields',
+import { LightningElement, api } from 'lwc';
+import getFields from '@salesforce/apex/FieldDescriptionController.getFields';
 
-  async run($) {
-    const { object } = $.step.parameters;
+export default class ListFields extends LightningElement {
+  @api objectApiName;
+  @api selectedFields = [];
 
-    if (!object) return { data: [] };
-
-    const response = await $.http.get(
-      `/services/data/v56.0/sobjects/${object}/describe`
-    );
-
-    const fields = response.data.fields.map((field) => {
+  @api
+  get fields() {
+    return this.selectedFields.map((field) => {
       return {
-        value: field.name,
-        name: field.label,
+        value: field,
+        name: this.getFieldLabel(field),
       };
     });
+  }
 
-    return { data: fields };
-  },
-};
+  connectedCallback() {
+    if (this.objectApiName) {
+      this.fetchFields();
+    }
+  }
+
+  async fetchFields() {
+    try {
+      const response = await getFields({ objectApiName: this.objectApiName });
+      this.selectedFields = response.fields.map((field) => field.Name);
+    } catch (error) {
+      console.error('Error fetching fields: ', error);
+    }
+  }
+
+  getFieldLabel(fieldName) {
+    const field = this.getField(fieldName);
+    return field ? field.Label : '';
+  }
+
+  getField(fieldName) {
+    return this.objectApiName
+      ? this.getFieldsMetadata().find((field) => field.Name === fieldName)
+      : null;
+  }
+
+  getFieldsMetadata() {
+    return (
+      this.objectApiName &&
+      JSON.parse(JSON.stringify(this.objectInfo.fields))
+    );
+  }
+
+  get objectInfo() {
+    return (
+      this.objectApiName &&
+      JSON.parse(JSON.stringify(this.template.querySelector('c-object-info').data))
+    );
+  }
+}

7a0c933e661a2485c8b5bd464e97f58954e5de88
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:43 +0100

    improve

--- packages/backend/src/apps/gitlab/triggers/deployment-event/deployment_event.js
+++ packages/backend/src/apps/gitlab/triggers/deployment-event/deployment_event.js
@@ -1,16 +1,34 @@
-// https://docs.gitlab.com/ee/user/project/integrations/webhook_events.html#deployment-events
+// Configuration for a successful GitLab deployment
 
-export default {
+const deploymentEventConfig = {
+  // The kind of GitLab event that triggered this configuration
   object_kind: 'deployment',
+  
+  // The status of the deployment
   status: 'success',
+
+  // The timestamp when the status was last changed
   status_changed_at: '2021-04-28 21:50:00 +0200',
+
+  // The ID of the deployment
   deployment_id: 15,
+
+  // The ID of the deployable object
   deployable_id: 796,
-  deployable_url:
-    'http://10.126.0.2:3000/root/test-deployment-webhooks/-/jobs/796',
+
+  // The URL for the deployable object
+  deployable_url: 'http://10.126.0.2:3000/root/test-deployment-webhooks/-/jobs/796',
+
+  // The name of the environment where the deployment was made
   environment: 'staging',
+
+  // The slug for the environment where the deployment was made
   environment_slug: 'staging',
+
+  // The external URL for the environment where the deployment was made
   environment_external_url: 'https://staging.example.com',
+
+  // Detailed information about the project where the deployment was made
   project: {
     id: 30,
     name: 'test-deployment-webhooks',
@@ -29,17 +47,27 @@ export default {
     ssh_url: 'ssh://vlad@10.126.0.2:2222/root/test-deployment-webhooks.git',
     http_url: 'http://10.126.0.2:3000/root/test-deployment-webhooks.git',
   },
+
+  // The short SHA for the commit associated with the deployment
   short_sha: '279484c0',
+
+  // Information about the user who triggered the deployment
   user: {
     id: 1,
     name: 'Administrator',
     username: 'root',
-    avatar_url:
-      'https://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon',
+    avatar_url: 'https://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon',
     email: 'admin@example.com',
   },
+
+  // The URL for the user who triggered the deployment
   user_url: 'http://10.126.0.2:3000/root',
-  commit_url:
-    'http://10.126.0.2:3000/root/test-deployment-webhooks/-/commit/279484c09fbe69ededfced8c1bb6e6d24616b468',
+
+  // The URL for the commit associated with the deployment
+  commit_url: 'http://10.126.0.2:3000/root/test-deployment-webhooks/-/commit/279484c09fbe69ededfced8c1bb6e6d24616b468',
+
+  // The title of the commit associated with the deployment
   commit_title: 'Add new file',
 };
+
+module.exports = deploymentEventConfig;

fdfc5fff6728da48a215ab05065762e2fe059db2
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:44 +0100

    improve

--- packages/backend/src/apps/strava/auth/refresh-token.js
+++ packages/backend/src/apps/strava/auth/refresh-token.js
@@ -6,15 +6,28 @@ const refreshToken = async ($) => {
     refresh_token: $.auth.data.refreshToken,
   };
 
-  const { data } = await $.http.post('/v3/oauth/token', null, { params });
+  try {
+    const { data } = await $.http.post('/v3/oauth/token', null, { params });
+    const {
+      access_token,
+      expires_in,
+      expires_at,
+      token_type,
+      refresh_token,
+    } = data;
 
-  await $.auth.set({
-    accessToken: data.access_token,
-    expiresIn: data.expires_in,
-    expiresAt: data.expires_at,
-    tokenType: data.token_type,
-    refreshToken: data.refresh_token,
-  });
+    await $.auth.set({
+      accessToken: access_token,
+      expiresIn: expires_in,
+      expiresAt: expires_at,
+      tokenType: token_type,
+      refreshToken: refresh_token,
+    });
+  } catch (error) {
+    // Handle any errors that occur during the execution of the function
+    console.error('An error occurred while refreshing the token:', error);
+  }
 };
 
 export default refreshToken;
+

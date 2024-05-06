78d4242ef9163290140e60c4376fcf404c73d459
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:27 +0100

    improve

--- packages/backend/src/apps/discord/common/add-auth-header.js
+++ packages/backend/src/apps/discord/common/add-auth-header.js
@@ -1,10 +1,24 @@
-const addAuthHeader = ($, requestConfig) => {
-  const { tokenType, botToken } = $.auth.data;
-  if (tokenType && botToken) {
-    requestConfig.headers.Authorization = `Bot ${botToken}`;
+const addAuthHeader = (authData, requestConfig) => {
+  if (authData && authData.tokenType && authData.botToken) {
+    requestConfig.headers = requestConfig.headers || {};
+    requestConfig.headers.Authorization = `Bot ${authData.botToken}`;
   }
 
   return requestConfig;
 };
+
+const authData = {
+  tokenType: 'Bearer',
+  botToken: 'my-bot-token'
+};
+
+const requestConfig = {
+  url: 'https://api.example.com/data',
+  method: 'GET'
+};
+
+const updatedConfig = addAuthHeader(authData, requestConfig);
+
+// Now you can use updatedConfig to make the authenticated request
 
 export default addAuthHeader;

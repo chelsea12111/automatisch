e2c90f479c336bfd244f4c9b645e702f248eb1e0
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:36 +0100

    improve

--- packages/backend/src/apps/formatter/dynamic-fields/list-transform-options/date-time/format-date-time.js
+++ packages/backend/src/apps/formatter/dynamic-fields/list-transform-options/date-time/format-date-time.js
@@ -1,51 +1,46 @@
 import formatOptions from './options/format.js';
 import timezoneOptions from './options/timezone.js';
 
-const formatDateTime = [
-  {
+const formatDateTime = {
+  input: {
     label: 'Input',
-    key: 'input',
     type: 'string',
     required: true,
     description: 'The datetime you want to format.',
     variables: true,
   },
-  {
+  fromFormat: {
     label: 'From Format',
-    key: 'fromFormat',
     type: 'dropdown',
     required: true,
     description: 'The format of the input.',
     variables: true,
     options: formatOptions,
   },
-  {
+  fromTimezone: {
     label: 'From Timezone',
-    key: 'fromTimezone',
     type: 'dropdown',
     required: true,
     description: 'The timezone of the input.',
     variables: true,
     options: timezoneOptions,
   },
-  {
+  toFormat: {
     label: 'To Format',
-    key: 'toFormat',
     type: 'dropdown',
     required: true,
     description: 'The format of the output.',
     variables: true,
     options: formatOptions,
   },
-  {
+  toTimezone: {
     label: 'To Timezone',
-    key: 'toTimezone',
     type: 'dropdown',
     required: true,
     description: 'The timezone of the output.',
     variables: true,
     options: timezoneOptions,
   },
-];
+};
 
 export default formatDateTime;

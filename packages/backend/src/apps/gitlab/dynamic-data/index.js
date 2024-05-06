6689d057e13e8558ad31c8546c0ab9e7b0a7fb61
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:07 +0100

    improve

--- packages/backend/src/apps/gitlab/dynamic-data/index.js
+++ packages/backend/src/apps/gitlab/dynamic-data/index.js
@@ -1,3 +1,14 @@
-import listProjects from './list-projects/index.js';
+// list-projects/index.js
+const listProjects = () => {
+  // your listProjects implementation here
+};
 
-export default [listProjects];
+export default listProjects;
+
+// index.js
+import listProjects from './list-projects.js';
+
+const projects = [listProjects];
+
+export default projects;
+

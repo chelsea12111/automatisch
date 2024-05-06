9fe863ad2727668a3e6ecb86168e2c5eb85215d0
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:23 +0100

    improve

--- packages/backend/src/apps/scheduler/common/get-next-cron-date-time.js
+++ packages/backend/src/apps/scheduler/common/get-next-cron-date-time.js
@@ -1,8 +1,21 @@
 import { DateTime } from 'luxon';
 import cronParser from 'cron-parser';
 
+/**
+ * Gets the next date/time for the given cron string.
+ * @param {string} cronString - The cron string to parse.
+ * @returns {DateTime} The next date/time for the given cron string.
+ */
 export default function getNextCronDateTime(cronString) {
+  // Check if cronString is not empty
+  if (!cronString) {
+    throw new Error('cronString cannot be empty');
+  }
+
+  // Parse the cron string
   const cronDate = cronParser.parseExpression(cronString);
+
+  // Get the next matching date/time
   const matchingNextCronDateTime = cronDate.next();
   const matchingNextDateTime = DateTime.fromJSDate(
     matchingNextCronDateTime.toDate()

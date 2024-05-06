adb751b555b86cb469ac4ab1ff3da9f0fc3bd4c2
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:54 +0100

    improve

--- packages/backend/src/apps/xero/auth/refresh-token.js
+++ packages/backend/src/apps/xero/auth/refresh-token.js
@@ -1,39 +1,39 @@
 import { URLSearchParams } from 'node:url';
+import axios, { AxiosResponse } from 'axios';
 
 import authScope from '../common/auth-scope.js';
 
-const refreshToken = async ($) => {
+const refreshToken = async (config: any): Promise<AxiosResponse> => {
+  const { clientId, clientSecret, refreshToken } = config.auth.data;
+
   const headers = {
-    Authorization: `Basic ${Buffer.from(
-      $.auth.data.clientId + ':' + $.auth.data.clientSecret
-    ).toString('base64')}`,
+    Authorization: `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
     'Content-Type': 'application/x-www-form-urlencoded',
   };
 
   const params = new URLSearchParams({
     grant_type: 'refresh_token',
-    refresh_token: $.auth.data.refreshToken,
+    refresh_token: refreshToken,
   });
 
-  const { data } = await $.http.post(
+  const response = await axios.post(
     'https://identity.xero.com/connect/token',
     params.toString(),
     {
       headers,
-      additionalProperties: {
-        skipAddingAuthHeader: true,
-      },
     }
   );
 
-  await $.auth.set({
-    accessToken: data.access_token,
-    refreshToken: data.refresh_token,
-    expiresIn: data.expires_in,
-    idToken: data.id_token,
+  await config.auth.set({
+    accessToken: response.data.access_token,
+    refreshToken: response.data.refresh_token,
+    expiresIn: response.data.expires_in,
+    idToken: response.data.id_token,
     scope: authScope.join(' '),
-    tokenType: data.token_type,
+    tokenType: response.data.token_type,
   });
+
+  return response;
 };
 
 export default refreshToken;

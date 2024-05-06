e19de19e3e4d2a65c6677ca6d9acb1e886318186
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:28 +0100

    improve

--- packages/backend/src/apps/mattermost/actions/send-a-message-to-channel/index.js
+++ packages/backend/src/apps/mattermost/actions/send-a-message-to-channel/index.js
@@ -4,38 +4,28 @@ import postMessage from './post-message.js';
 export default defineAction({
   name: 'Send a message to channel',
   key: 'sendMessageToChannel',
-  description: 'Sends a message to a channel you specify.',
+  description: 'Sends a message to the specified channel.',
   arguments: [
     {
       label: 'Channel',
       key: 'channel',
       type: 'dropdown',
       required: true,
-      description: 'Pick a channel to send the message to.',
-      variables: true,
-      source: {
-        type: 'query',
-        name: 'getDynamicData',
-        arguments: [
-          {
-            name: 'key',
-            value: 'listChannels',
-          },
-        ],
-      },
+      description: 'Select the channel to send the message to.',
+      dynamic: 'listChannels',
     },
     {
       label: 'Message text',
       key: 'message',
       type: 'string',
       required: true,
-      description: 'The content of your new message.',
-      variables: true,
+      description: 'Enter the content of your message.',
+      dynamic: true,
     },
   ],
 
   async run($) {
-    const message = await postMessage($);
+    const message = await postMessage({ channel: $.channel, text: $.message });
 
     return message;
   },

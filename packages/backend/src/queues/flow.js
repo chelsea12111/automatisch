998e90ed2e854ff1c92dbc41faa9553572c54cb5
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:10 +0100

    improve

--- packages/backend/src/queues/flow.js
+++ packages/backend/src/queues/flow.js
@@ -1,31 +1,38 @@
-import process from 'process';
 import { Queue } from 'bullmq';
 import redisConfig from '../config/redis.js';
 import logger from '../helpers/logger.js';
+import { exit } from 'process';
 
 const CONNECTION_REFUSED = 'ECONNREFUSED';
 
-const redisConnection = {
-  connection: redisConfig,
+const createQueue = (queueName) => {
+  const redisConnection = {
+    connection: redisConfig,
+  };
+
+  const queue = new Queue(queueName, redisConnection);
+
+  queue.on('error', (error) => {
+    if (error.code === CONNECTION_REFUSED) {
+      logger.error(
+        `Make sure Redis is installed and running. Error in queue '${queueName}':`,
+        error
+      );
+
+      exit(1);
+    }
+
+    logger.error(`Error happened in queue '${queueName}':`, error);
+  });
+
+  return queue;
 };
 
-const flowQueue = new Queue('flow', redisConnection);
+const flowQueue = createQueue('flow');
 
 process.on('SIGTERM', async () => {
   await flowQueue.close();
-});
-
-flowQueue.on('error', (error) => {
-  if (error.code === CONNECTION_REFUSED) {
-    logger.error(
-      'Make sure you have installed Redis and it is running.',
-      error
-    );
-
-    process.exit();
-  }
-
-  logger.error('Error happened in flow queue!', error);
+  exit(0);
 });
 
 export default flowQueue;

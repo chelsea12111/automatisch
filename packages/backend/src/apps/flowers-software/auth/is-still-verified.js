3fe39323942d5147594231214cff31c83fe83180
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:56 +0100

    improve

--- packages/backend/src/apps/flowers-software/auth/is-still-verified.js
+++ packages/backend/src/apps/flowers-software/auth/is-still-verified.js
@@ -1,9 +1,13 @@
-import verifyCredentials from './verify-credentials.js';
+import { verifyCredentials } from './verify-credentials.js';
 
-const isStillVerified = async ($) => {
-  await verifyCredentials($);
-
-  return true;
+const isStillVerified = async (userData) => {
+  try {
+    await verifyCredentials(userData);
+    return true;
+  } catch (error) {
+    console.error('Error during verification:', error);
+    return false;
+  }
 };
 
 export default isStillVerified;

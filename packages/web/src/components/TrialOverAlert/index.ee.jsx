aa0c242fca986edac8721ed4a3a309faa9409360
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:52 +0100

    improve

--- packages/web/src/components/TrialOverAlert/index.ee.jsx
+++ packages/web/src/components/TrialOverAlert/index.ee.jsx
@@ -1,17 +1,27 @@
 import * as React from 'react';
 import Alert from '@mui/material/Alert';
 import Typography from '@mui/material/Typography';
-
-import * as URLS from 'config/urls';
 import { generateInternalLink } from 'helpers/translationValues';
-import useUserTrial from 'hooks/useUserTrial.ee';
-import useFormatMessage from 'hooks/useFormatMessage';
+import { useUserTrial } from 'hooks/useUserTrial';
+import { useFormatMessage } from 'hooks/useFormatMessage';
+import { URLS } from 'config/urls';
 
-export default function TrialOverAlert() {
+interface TrialOverAlertProps {
+  trialOver: boolean;
+}
+
+const TrialOverAlertMemo = ({ trialOver }: TrialOverAlertProps) => {
   const formatMessage = useFormatMessage();
   const trialStatus = useUserTrial();
 
-  if (!trialStatus || !trialStatus.over) return <React.Fragment />;
+  const link = React.useMemo(() => {
+    if (trialStatus && trialStatus.over) {
+      return generateInternalLink(URLS.SETTINGS_PLAN_UPGRADE);
+    }
+    return '';
+  }, [generateInternalLink, trialStatus, URLS.SETTINGS_PLAN_UPGRADE]);
+
+  if (!trialOver || !trialStatus || !trialStatus.over) return <React.Fragment />;
 
   return (
     <Alert
@@ -22,10 +32,12 @@ export default function TrialOverAlert() {
       }}
     >
       <Typography variant="subtitle2" sx={{ lineHeight: 1.5 }}>
-        {formatMessage('trialOverAlert.text', {
-          link: generateInternalLink(URLS.SETTINGS_PLAN_UPGRADE),
-        })}
+        {formatMessage('trialOverAlert.text', { link })}
       </Typography>
     </Alert>
   );
-}
+};
+
+const TrialOverAlert = React.memo(TrialOverAlertMemo);
+
+export default TrialOverAlert;

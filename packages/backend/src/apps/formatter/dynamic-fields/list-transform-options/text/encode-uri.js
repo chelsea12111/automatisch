e618e67931ed351e875f3ab9075614696d60d6b6
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:38 +0100

    improve

--- packages/backend/src/apps/formatter/dynamic-fields/list-transform-options/text/encode-uri.js
+++ packages/backend/src/apps/formatter/dynamic-fields/list-transform-options/text/encode-uri.js
@@ -1,11 +1,22 @@
-const encodeUri = [
+type FieldType = 'string' | 'number' | 'boolean' | 'object' | 'array';
+
+interface FieldConfig {
+  label: string;
+  key: string;
+  required?: boolean;
+  description?: string;
+  variables?: boolean;
+  type: FieldType;
+}
+
+const encodeUri: FieldConfig[] = [
   {
     label: 'Input',
     key: 'input',
-    type: 'string',
     required: true,
     description: 'URI to encode',
     variables: true,
+    type: 'string',
   },
 ];
 

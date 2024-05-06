ab3adb59cf6501dbfb0bd36dce362ebf5d3c7a4b
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:35 +0100

    improve

--- packages/backend/src/apps/filter/actions/continue/index.js
+++ packages/backend/src/apps/filter/actions/continue/index.js
@@ -9,36 +9,6 @@ const isLessThanOrEqual = (a, b) => Number(a) <= Number(b);
 const contains = (a, b) => a.includes(b);
 const doesNotContain = (a, b) => !contains(a, b);
 
-const shouldContinue = (orGroups) => {
-  let atLeastOneGroupMatches = false;
-
-  for (const group of orGroups) {
-    let groupMatches = true;
-
-    for (const condition of group.and) {
-      const conditionMatches = operate(
-        condition.operator,
-        condition.key,
-        condition.value
-      );
-
-      if (!conditionMatches) {
-        groupMatches = false;
-
-        break;
-      }
-    }
-
-    if (groupMatches) {
-      atLeastOneGroupMatches = true;
-
-      break;
-    }
-  }
-
-  return atLeastOneGroupMatches;
-};
-
 const operators = {
   equal: isEqual,
   not_equal: isNotEqual,
@@ -46,12 +16,27 @@ const operators = {
   less_than: isLessThan,
   greater_than_or_equal: isGreaterThanOrEqual,
   less_than_or_equal: isLessThanOrEqual,
-  contains: contains,
-  not_contains: doesNotContain,
+  contains,
+  not_contains,
+};
+
+const isOperatorValid = (operator) => {
+  return Object.keys(operators).includes(operator);
 };
 
 const operate = (operation, a, b) => {
+  if (!isOperatorValid(operation)) {
+    throw new Error(`Invalid operator: ${operation}`);
+  }
   return operators[operation](a, b);
+};
+
+const shouldContinue = (orGroups) => {
+  return orGroups.some((group) => {
+    return group.and.every((condition) => {
+      return operate(condition.operator, condition.key, condition.value);
+    });
+  });
 };
 
 export default defineAction({
@@ -62,6 +47,10 @@ export default defineAction({
 
   async run($) {
     const orGroups = $.step.parameters.or;
+
+    if (!Array.isArray(orGroups) || !orGroups.length) {
+      throw new Error('Invalid `or` parameter');
+    }
 
     const matchingGroups = orGroups.reduce((groups, group) => {
       const matchingConditions = group.and.filter((condition) =>

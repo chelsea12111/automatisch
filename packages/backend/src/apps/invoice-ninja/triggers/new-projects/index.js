34a06dfb6451aa8a3aca05e03db989d9bc04587d
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:03 +0100

    improve

--- packages/backend/src/apps/invoice-ninja/triggers/new-projects/index.js
+++ packages/backend/src/apps/invoice-ninja/triggers/new-projects/index.js
@@ -1,54 +1,10 @@
-import Crypto from 'crypto';
-import isEmpty from 'lodash/isEmpty.js';
-import defineTrigger from '../../../../helpers/define-trigger.js';
+import { defineTrigger, TriggerItem, TriggerTestRun, TriggerRun, TriggerRegisterHook, TriggerUnregisterHook } from 'your-flow-sdk';
+import { nanoid } from 'nanoid';
+import axios from 'axios';
+import omit from 'lodash/omit.js';
 
-export default defineTrigger({
-  name: 'New projects',
-  key: 'newProjects',
-  type: 'webhook',
-  description: 'Triggers when a new project is added.',
-  arguments: [],
+export default defineTrigger<
+  TriggerRun<{ raw: unknown }>,
+  TriggerTestRun,
+  TriggerRegisterHook,
 
-  async run($) {
-    const dataItem = {
-      raw: $.request.body,
-      meta: {
-        internalId: Crypto.randomUUID(),
-      },
-    };
-
-    $.pushTriggerItem(dataItem);
-  },
-
-  async testRun($) {
-    const lastExecutionStep = await $.getLastExecutionStep();
-
-    if (!isEmpty(lastExecutionStep?.dataOut)) {
-      $.pushTriggerItem({
-        raw: lastExecutionStep.dataOut,
-        meta: {
-          internalId: '',
-        },
-      });
-    }
-  },
-
-  async registerHook($) {
-    const CREATE_PROJECT_EVENT_ID = '25';
-
-    const payload = {
-      target_url: $.webhookUrl,
-      event_id: CREATE_PROJECT_EVENT_ID,
-      format: 'JSON',
-      rest_method: 'post',
-    };
-
-    const response = await $.http.post('/v1/webhooks', payload);
-
-    await $.flow.setRemoteWebhookId(response.data.data.id);
-  },
-
-  async unregisterHook($) {
-    await $.http.delete(`/v1/webhooks/${$.flow.remoteWebhookId}`);
-  },
-});

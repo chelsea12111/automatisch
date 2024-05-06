c7a175a8a5af7f03091226fea7f3e017c8803be6
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:01 +0100

    improve

--- packages/backend/src/apps/mattermost/common/get-current-user.js
+++ packages/backend/src/apps/mattermost/common/get-current-user.js
@@ -1,7 +1,13 @@
-const getCurrentUser = async ($) => {
-  const response = await $.http.get('/api/v4/users/me');
-  const currentUser = response.data;
-  return currentUser;
+const getCurrentUser = async (http) => {
+  try {
+    const response = await http.get('/api/v4/users/me');
+    const currentUser = response.data;
+    return currentUser;
+  } catch (error) {
+    console.error('Error fetching current user:', error);
+    throw error;
+  }
 };
 
 export default getCurrentUser;
+

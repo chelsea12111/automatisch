fd43d62b85bd6eaccff966243c74965979ef6260
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:53 +0100

    improve

--- packages/web/src/graphql/mutations/create-flow.js
+++ packages/web/src/graphql/mutations/create-flow.js
@@ -1,7 +1,15 @@
 import { gql } from '@apollo/client';
+
+// Define the shape of the input argument
+interface CreateFlowInput {
+  name: string;
+  // Add any other fields that are required for creating a flow
+}
+
+// Use the defined input type in the mutation
 export const CREATE_FLOW = gql`
-  mutation CreateFlow($input: CreateFlowInput) {
-    createFlow(input: $input) {
+  mutation CreateFlow($createFlowInput: CreateFlowInput!) {
+    createFlow(input: $createFlowInput) {
       id
       name
     }

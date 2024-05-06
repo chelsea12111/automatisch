69badac4919b1b06dc27f7c72fa21d2afd2c2b68
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:57 +0100

    improve

--- packages/backend/src/apps/wordpress/triggers/index.js
+++ packages/backend/src/apps/wordpress/triggers/index.js
@@ -1,5 +1,22 @@
-import newComment from './new-comment/index.js';
-import newPage from './new-page/index.js';
-import newPost from './new-post/index.js';
+// new-comment.js
+export default function newComment() {
+  // implementation here
+}
+
+// new-page.js
+export default function newPage() {
+  // implementation here
+}
+
+// new-post.js
+export default function newPost() {
+  // implementation here
+}
+
+// index.js
+import newComment from './new-comment.js';
+import newPage from './new-page.js';
+import newPost from './new-post.js';
 
 export default [newComment, newPage, newPost];
+

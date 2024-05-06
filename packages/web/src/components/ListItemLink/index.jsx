e077cad38520ab60272994805d8bea4f450f27d7
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:43 +0100

    improve

--- packages/web/src/components/ListItemLink/index.jsx
+++ packages/web/src/components/ListItemLink/index.jsx
@@ -3,33 +3,69 @@ import { useMatch } from 'react-router-dom';
 import ListItem from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import { Link } from 'react-router-dom';
-export default function ListItemLink(props) {
-  const { icon, primary, to, onClick, 'data-test': dataTest, target } = props;
+import { Link as RouterLink } from 'react-router-dom';
+
+export default function ListItemLink({
+  icon,
+  primary,
+  to,
+  onClick,
+  'data-test': dataTest,
+  target,
+}: {
+  icon: React.ReactNode;
+  primary: string;
+  to: string;
+  onClick?: () => void;
+  'data-test'?: string;
+  target?: string;
+}) {
   const selected = useMatch({ path: to, end: true });
-  const CustomLink = React.useMemo(
-    () =>
-      React.forwardRef(function InLineLink(linkProps, ref) {
-        try {
-          // challenge the link to check if it's absolute URL
-          new URL(to); // should throw an error if it's not an absolute URL
+
+  const CustomLink = React.useCallback(
+    (linkProps: React.ComponentProps<typeof RouterLink>) => {
+      const [, pathname] = to.split('#') || [];
+
+      try {
+        // challenge the link to check if it's absolute URL
+        new URL(to); // should throw an error if it's not an absolute URL
+        return (
+          <a
+            {...linkProps}
+            href={to}
+            target={target}
+            rel="noopener noreferrer"
+          />
+        );
+      } catch (error) {
+        if (error instanceof TypeError) {
           return (
-            <a
+            <RouterLink
+              ref={linkProps.innerRef}
+              to={pathname || to}
+              onClick={(event) => {
+                if (onClick) {
+                  onClick();
+                }
+
+                // prevent the default click behavior for non-absolute URLs
+                if (!pathname) {
+                  event.preventDefault();
+                }
+              }}
               {...linkProps}
-              ref={ref}
-              href={to}
-              target={target}
-              rel="noopener noreferrer"
             />
           );
-        } catch {
-          return <Link ref={ref} {...linkProps} to={to} />;
         }
-      }),
-    [to],
+
+        throw error;
+      }
+    },
+    [to, onClick, target],
   );
+
   return (
-    <li>
+    <li key={to}>
       <ListItem
         component={CustomLink}
         sx={{ pl: { xs: 2, sm: 3 } }}

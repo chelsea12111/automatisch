6966b60c839ea2a0d31c3dd84d250c82605235a4
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:31 +0100

    improve

--- packages/backend/src/graphql/mutations/delete-user.ee.js
+++ packages/backend/src/graphql/mutations/delete-user.ee.js
@@ -2,13 +2,14 @@ import { Duration } from 'luxon';
 import User from '../../models/user.js';
 import deleteUserQueue from '../../queues/delete-user.ee.js';
 
-const deleteUser = async (_parent, params, context) => {
+const deleteUser = async (_parent, { input: { id } }, context) => {
+  // Check if the user has permission to delete a user
   context.currentUser.can('delete', 'User');
 
-  const id = params.input.id;
-
+  // Delete the user from the database
   await User.query().deleteById(id);
 
+  // Schedule a job to delete the user's data after 30 days
   const jobName = `Delete user - ${id}`;
   const jobPayload = { id };
   const millisecondsFor30Days = Duration.fromObject({ days: 30 }).toMillis();

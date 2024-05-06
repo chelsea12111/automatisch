84907b712708c8611c390ae32da8b2584dd6131a
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:43 +0100

    improve

--- packages/backend/src/db/migrations/20240227164849_create_datastore_model.js
+++ packages/backend/src/db/migrations/20240227164849_create_datastore_model.js
@@ -1,16 +1,22 @@
-export async function up(knex) {
-  return knex.schema.createTable('datastore', (table) => {
+import { Knex } from 'knex';
+
+const tableName = 'datastore';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(tableName, (table) => {
     table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
-    table.string('key').notNullable();
-    table.string('value');
-    table.string('scope').notNullable();
+    table.string('key', 255).notNullable();
+    table.text('value');
+    table.string('scope', 255).notNullable();
     table.uuid('scope_id').notNullable();
-    table.index(['key', 'scope', 'scope_id']);
+
+    table.unique(['key', 'scope', 'scope_id'], { indexName: 'unique_key_scope_scope_id' });
+    table.index(['scope_id'], { indexName: 'index_scope_id' });
 
     table.timestamps(true, true);
   });
 }
 
-export async function down(knex) {
-  return knex.schema.dropTable('datastore');
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable(tableName);
 }

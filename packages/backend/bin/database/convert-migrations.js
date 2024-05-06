df2c9b359903cf450b84923920059ffe23d24a2d
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:20 +0100

    improve

--- packages/backend/bin/database/convert-migrations.js
+++ packages/backend/bin/database/convert-migrations.js
@@ -1,31 +1,43 @@
-import appConfig from '../../src/config/app.js';
-import logger from '../../src/helpers/logger.js';
-import '../../src/config/orm.js';
-import { client as knex } from '../../src/config/database.js';
+import appConfig from '../../src/config/app';
+import logger from '../../src/helpers/logger';
+import '../../src/config/orm';
+import { Knex } from 'knex';
+import { Client } from 'knex/types/drivers/pg/client-base';
 
-export const renameMigrationsAsJsFiles = async () => {
+export const renameMigrationsAsJsFiles = async (): Promise<void> => {
   if (!appConfig.isDev) {
     return;
   }
 
   try {
-    const tableExists = await knex.schema.hasTable('knex_migrations');
+    const knexClient = knex as Client<any, Knex.TransactionClient<any>>;
+    const tableExists = await knexClient.schema.hasTable('knex_migrations');
 
     if (tableExists) {
-      await knex('knex_migrations')
+      await knexClient('knex_migrations')
         .where('name', 'like', '%.ts')
         .update({
-          name: knex.raw("REPLACE(name, '.ts', '.js')"),
+          name: knexClient.raw("REPLACE(name, '.ts', '.js')"),
         });
+
       logger.info(
         `Migration file names with typescript renamed as JS file names!`
       );
     }
   } catch (err) {
     logger.error(err.message);
+  } finally {
+    try {
+      if (knex && knex.client) {
+        await (knex.client as any).destroy();
+      }
+    } catch (err) {
+      logger.error(err.message);
+    }
   }
-
-  await knex.destroy();
 };
 
-renameMigrationsAsJsFiles();
+(async () => {
+  await renameMigrationsAsJsFiles();
+})();
+

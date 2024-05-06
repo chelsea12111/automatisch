af63f2c2871f2ae81760d62a30b9e6ae10d65b13
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:38 +0100

    improve

--- packages/backend/src/apps/salesforce/auth/generate-auth-url.js
+++ packages/backend/src/apps/salesforce/auth/generate-auth-url.js
@@ -1,9 +1,11 @@
 import qs from 'qs';
 
-export default async function generateAuthUrl($) {
-  const oauthRedirectUrlField = $.app.auth.fields.find(
-    (field) => field.key == 'oAuthRedirectUrl'
-  );
+export default async function generateAuthUrl({ app: { auth: { fields } } }) {
+  const oauthRedirectUrlField = fields.find(field => field.key === 'oAuthRedirectUrl');
+  if (!oauthRedirectUrlField) {
+    throw new Error('Missing oAuthRedirectUrl field');
+  }
+
   const redirectUri = oauthRedirectUrlField.value;
   const searchParams = qs.stringify({
     client_id: $.auth.data.consumerKey,
@@ -15,3 +17,4 @@ export default async function generateAuthUrl($) {
     url: `${$.auth.data.oauth2Url}/authorize?${searchParams}`,
   });
 }
+

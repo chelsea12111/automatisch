328d2d2687bd25f60ce069748479edc6d8e4a44d
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:51 +0100

    improve

--- packages/backend/src/apps/ghost/auth/index.js
+++ packages/backend/src/apps/ghost/auth/index.js
@@ -1,32 +1,34 @@
 import verifyCredentials from './verify-credentials.js';
 import isStillVerified from './is-still-verified.js';
 
-export default {
-  fields: [
-    {
-      key: 'instanceUrl',
-      label: 'Instance URL',
-      type: 'string',
-      required: true,
-      readOnly: false,
-      value: null,
-      placeholder: null,
-      description: null,
-      clickToCopy: false,
-    },
-    {
-      key: 'apiKey',
-      label: 'Admin API Key',
-      type: 'string',
-      required: true,
-      readOnly: false,
-      value: null,
-      placeholder: null,
-      description: null,
-      clickToCopy: false,
-    },
-  ],
+const fields = [
+  {
+    key: 'instanceUrl',
+    label: 'Instance URL',
+    type: 'string',
+    required: true,
+    readOnly: false,
+    value: null,
+    placeholder: null,
+    description: null,
+    clickToCopy: false,
+  },
+  {
+    key: 'apiKey',
+    label: 'Admin API Key',
+    type: 'string',
+    required: true,
+    readOnly: false,
+    value: null,
+    placeholder: null,
+    description: null,
+    clickToCopy: false,
+  },
+];
 
+const config = {
   verifyCredentials,
   isStillVerified,
 };
+
+export default { fields, ...config };

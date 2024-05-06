550d881803236ce9887a87f700a5faeec2b1dea3
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:20 +0100

    improve

--- packages/backend/src/apps/carbone/actions/index.js
+++ packages/backend/src/apps/carbone/actions/index.js
@@ -1,3 +1,9 @@
+// import the addTemplate module
 import addTemplate from './add-template/index.js';
 
-export default [addTemplate];
+// create an array containing the addTemplate module
+const templates = [addTemplate];
+
+// export the templates array as the default export
+export default templates;
+

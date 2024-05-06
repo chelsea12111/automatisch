6293c57c173fb5fac97d7d3e8ddafb3ed77e4fac
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:39 +0100

    improve

--- packages/backend/src/apps/filter/index.js
+++ packages/backend/src/apps/filter/index.js
@@ -1,14 +1,21 @@
 import defineApp from '../../helpers/define-app.js';
 import actions from './actions/index.js';
 
+const APP_NAME = 'Filter';
+const APP_KEY = 'filter';
+const ICON_URL = '{BASE_URL}/apps/filter/assets/favicon.svg';
+const AUTH_DOC_URL = '{DOCS_URL}/apps/filter/connection';
+const SUPPORTS_CONNECTIONS = false;
+const PRIMARY_COLOR = '#001F52'; // use hex code for color representation
+
 export default defineApp({
-  name: 'Filter',
-  key: 'filter',
-  iconUrl: '{BASE_URL}/apps/filter/assets/favicon.svg',
-  authDocUrl: '{DOCS_URL}/apps/filter/connection',
-  supportsConnections: false,
+  name: APP_NAME,
+  key: APP_KEY,
+  iconUrl: ICON_URL,
+  authDocUrl: AUTH_DOC_URL,
+  supportsConnections: SUPPORTS_CONNECTIONS,
   baseUrl: '',
   apiBaseUrl: '',
-  primaryColor: '001F52',
+  primaryColor: PRIMARY_COLOR,
   actions,
 });

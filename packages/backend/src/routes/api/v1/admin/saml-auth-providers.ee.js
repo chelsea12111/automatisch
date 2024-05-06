24a476a9cdd82290eec6b6bec129dba85211b5b3
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:06 +0100

    improve

--- packages/backend/src/routes/api/v1/admin/saml-auth-providers.ee.js
+++ packages/backend/src/routes/api/v1/admin/saml-auth-providers.ee.js
@@ -1,36 +1,15 @@
 import { Router } from 'express';
 import asyncHandler from 'express-async-handler';
-import { authenticateUser } from '../../../../helpers/authentication.js';
-import { authorizeAdmin } from '../../../../helpers/authorization.js';
-import { checkIsEnterprise } from '../../../../helpers/check-is-enterprise.js';
-import getSamlAuthProvidersAction from '../../../../controllers/api/v1/admin/saml-auth-providers/get-saml-auth-providers.ee.js';
-import getSamlAuthProviderAction from '../../../../controllers/api/v1/admin/saml-auth-providers/get-saml-auth-provider.ee.js';
-import getRoleMappingsAction from '../../../../controllers/api/v1/admin/saml-auth-providers/get-role-mappings.ee.js';
+import { authenticateUser, authorizeAdmin, checkIsEnterprise } from '../../../../helpers/index.js';
+import * as samlAuthProvidersController from '../../../../controllers/api/v1/admin/saml-auth-providers/index.ee.js';
 
 const router = Router();
 
-router.get(
-  '/',
-  authenticateUser,
-  authorizeAdmin,
-  checkIsEnterprise,
-  asyncHandler(getSamlAuthProvidersAction)
-);
+const authMiddleware = [authenticateUser, authorizeAdmin, checkIsEnterprise];
 
-router.get(
-  '/:samlAuthProviderId',
-  authenticateUser,
-  authorizeAdmin,
-  checkIsEnterprise,
-  asyncHandler(getSamlAuthProviderAction)
-);
-
-router.get(
-  '/:samlAuthProviderId/role-mappings',
-  authenticateUser,
-  authorizeAdmin,
-  checkIsEnterprise,
-  asyncHandler(getRoleMappingsAction)
-);
+router.get('/', ...authMiddleware, asyncHandler(samlAuthProvidersController.getSamlAuthProviders));
+router.get('/:samlAuthProviderId', ...authMiddleware, asyncHandler(samlAuthProvidersController.getSamlAuthProvider));
+router.get('/:samlAuthProviderId/role-mappings', ...authMiddleware, asyncHandler(samlAuthProvidersController.getRoleMappings));
 
 export default router;
+

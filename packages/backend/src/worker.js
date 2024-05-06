fa70f60f7da9dd5b6c03b1b4ec8d1be96de99365
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:31 +0100

    improve

--- packages/backend/src/worker.js
+++ packages/backend/src/worker.js
@@ -1,21 +1,48 @@
-import * as Sentry from './helpers/sentry.ee.js';
+// Import all helpers and configuration files first
+import { init, captureException } from './helpers/sentry.ee.js';
 import appConfig from './config/app.js';
-
-Sentry.init();
-
 import './config/orm.js';
-import './helpers/check-worker-readiness.js';
-import './workers/flow.js';
-import './workers/trigger.js';
-import './workers/action.js';
-import './workers/email.js';
-import './workers/delete-user.ee.js';
+import checkWorkerReadiness from './helpers/check-worker-readiness.js';
 
+// Import all worker files
+import flow from './workers/flow.js';
+import trigger from './workers/trigger.js';
+import action from './workers/action.js';
+import email from './workers/email.js';
+import deleteUser from './workers/delete-user.ee.js';
+
+// Import cloud-specific workers
 if (appConfig.isCloud) {
-  import('./workers/remove-cancelled-subscriptions.ee.js');
-  import('./queues/remove-cancelled-subscriptions.ee.js');
+  import removeCancelledSubscriptionsWorker from './workers/remove-cancelled-subscriptions.ee.js';
+  import removeCancelledSubscriptionsQueue from './queues/remove-cancelled-subscriptions.ee.js';
+
+  // Run cloud-specific workers
+  removeCancelledSubscriptionsWorker();
+  removeCancelledSubscriptionsQueue();
 }
 
-import telemetry from './helpers/telemetry/index.js';
+// Initialize Sentry
+init();
 
+// Check worker readiness
+checkWorkerReadiness();
+
+// Start worker functions
+flow();
+trigger();
+action();
+email();
+deleteUser();
+
+// Import and initialize telemetry
+import telemetry from './helpers/telemetry/index.js';
 telemetry.setServiceType('worker');
+
+// Handle errors with Sentry
+process.on('uncaughtException', (error) => {
+  captureException(error);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  captureException(reason);
+});

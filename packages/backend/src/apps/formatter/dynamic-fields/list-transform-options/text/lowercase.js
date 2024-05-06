3d5b8705a09328ff7bab6fc1c0fa84ab6753fcad
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:42 +0100

    improve

--- packages/backend/src/apps/formatter/dynamic-fields/list-transform-options/text/lowercase.js
+++ packages/backend/src/apps/formatter/dynamic-fields/list-transform-options/text/lowercase.js
@@ -1,12 +1,21 @@
-const lowercase = [
-  {
-    label: 'Input',
-    key: 'input',
-    type: 'string',
-    required: true,
-    description: 'Text that will be lowercased.',
-    variables: true,
-  },
-];
+interface FieldConfig {
+  label: string;
+  key: string;
+  type: string;
+  required?: boolean;
+  description?: string;
+  variables?: boolean;
+}
+
+const lowercaseField: FieldConfig = {
+  label: 'Input',
+  key: 'input',
+  type: 'string',
+  required: true,
+  description: 'Text that will be lowercased.',
+  variables: true,
+};
+
+const lowercase: FieldConfig[] = [lowercaseField];
 
 export default lowercase;

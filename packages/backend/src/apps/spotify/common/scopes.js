1c877247309f450940e15ed49ce3e2f2d771135c
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:09 +0100

    improve

--- packages/backend/src/apps/spotify/common/scopes.js
+++ packages/backend/src/apps/spotify/common/scopes.js
@@ -1,3 +1,7 @@
+// This module exports an array of Spotify API scopes that the app needs
+// to access the user's data and perform actions on their behalf.
+// For more information, see: https://developer.spotify.com/documentation/general/guides/scopes/
+
 const scopes = [
   'user-follow-read',
   'playlist-read-private',
@@ -7,7 +11,11 @@ const scopes = [
   'playlist-modify-private',
   'user-library-modify',
   'user-follow-modify',
-  'user-follow-read',
 ];
 
+// Sort the array alphabetically for easier reading and comparison
+scopes.sort();
+
+// Export the array as the default export of this module
 export default scopes;
+

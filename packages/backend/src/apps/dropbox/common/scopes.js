b1c4d368c89d51aafe659a1c8b9177e2933d5a84
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:09 +0100

    improve

--- packages/backend/src/apps/dropbox/common/scopes.js
+++ packages/backend/src/apps/dropbox/common/scopes.js
@@ -1,3 +1,15 @@
+// Array of scopes for API permissions
+const scopes: string[] = [
+  'account_info.read',
+  'files.metadata.read',
+  'files.content.write',
+  'files.content.read',
+];
+
+export { scopes };
+
+
+// Array of scopes for API permissions
 const scopes = [
   'account_info.read',
   'files.metadata.read',
@@ -5,4 +17,4 @@ const scopes = [
   'files.content.read',
 ];
 
-export default scopes;
+export { scopes };

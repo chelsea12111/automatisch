f8ad5a1268d912cdef20262c121a71dfd9f1f391
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:13 +0100

    improve

--- packages/backend/src/apps/formatter/actions/text/transformers/replace.js
+++ packages/backend/src/apps/formatter/actions/text/transformers/replace.js
@@ -1,10 +1,22 @@
-const replace = ($) => {
-  const input = $.step.parameters.input;
+import { StepParameters } from './step-parameters';
 
-  const find = $.step.parameters.find;
-  const replace = $.step.parameters.replace;
+const replace = (parameters: StepParameters): string => {
+  const { input, find, replace } = parameters;
+
+  if (typeof find !== 'string' || typeof replace !== 'string') {
+    throw new Error('The `find` and `replace` parameters must be strings');
+  }
 
   return input.replaceAll(find, replace);
 };
 
 export default replace;
+
+
+
+export interface StepParameters {
+  input: string;
+  find: string;
+  replace: string;
+}
+

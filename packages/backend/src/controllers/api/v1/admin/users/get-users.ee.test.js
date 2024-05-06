dd6643bf9f8bc6df4600f8f8eb63dd92601f3ce8
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:04 +0100

    improve

--- packages/backend/src/controllers/api/v1/admin/users/get-users.ee.test.js
+++ packages/backend/src/controllers/api/v1/admin/users/get-users.ee.test.js
@@ -1,15 +1,23 @@
-import { vi, describe, it, expect, beforeEach } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import request from 'supertest';
 import app from '../../../../../app';
 import createAuthTokenByUserId from '../../../../../helpers/create-auth-token-by-user-id';
 import { createRole } from '../../../../../../test/factories/role';
 import { createUser } from '../../../../../../test/factories/user';
-import getUsersMock from '../../../../../../test/mocks/rest/api/v1/admin/users/get-users.js';
+import { getUsersMock } from '../../../../../../test/mocks/rest/api/v1/admin/users/get-users.js';
 import * as license from '../../../../../helpers/license.ee.js';
 
-describe('GET /api/v1/admin/users', () => {
-  let currentUser, currentUserRole, anotherUser, anotherUserRole, token;
+let currentUser, currentUserRole, anotherUser, anotherUserRole, token;
 
+vi.mock('../../../../../helpers/license.ee.js', async () => {
+  const originalModule = await vi.importActual('../../../../../helpers/license.ee.js');
+  return {
+    ...originalModule,
+    hasValidLicense: vi.fn().mockResolvedValue(true),
+  };
+});
+
+describe('GET /api/v1/admin/users', () => {
   beforeEach(async () => {
     currentUserRole = await createRole({ key: 'admin' });
 
@@ -32,18 +40,14 @@ describe('GET /api/v1/admin/users', () => {
   });
 
   it('should return users data', async () => {
-    vi.spyOn(license, 'hasValidLicense').mockResolvedValue(true);
-
     const response = await request(app)
       .get('/api/v1/admin/users')
       .set('Authorization', token)
       .expect(200);
 
-    const expectedResponsePayload = await getUsersMock(
-      [anotherUser, currentUser],
-      [anotherUserRole, currentUserRole]
-    );
+    const expectedResponsePayload = await getUsersMock(currentUser, anotherUser, currentUserRole, anotherUserRole);
 
     expect(response.body).toEqual(expectedResponsePayload);
   });
 });
+

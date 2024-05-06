ec3209a6f72620828e2cf3ea0ffd161fe0328b79
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:57 +0100

    improve

--- packages/backend/src/apps/google-tasks/auth/index.js
+++ packages/backend/src/apps/google-tasks/auth/index.js
@@ -3,46 +3,41 @@ import verifyCredentials from './verify-credentials.js';
 import refreshToken from './refresh-token.js';
 import isStillVerified from './is-still-verified.js';
 
-export default {
-  fields: [
-    {
-      key: 'oAuthRedirectUrl',
-      label: 'OAuth Redirect URL',
-      type: 'string',
-      required: true,
-      readOnly: true,
-      value: '{WEB_APP_URL}/app/google-tasks/connections/add',
-      placeholder: null,
-      description:
-        'When asked to input a redirect URL in Google Cloud, enter the URL above.',
-      clickToCopy: true,
-    },
-    {
-      key: 'clientId',
-      label: 'Client ID',
-      type: 'string',
-      required: true,
-      readOnly: false,
-      value: null,
-      placeholder: null,
-      description: null,
-      clickToCopy: false,
-    },
-    {
-      key: 'clientSecret',
-      label: 'Client Secret',
-      type: 'string',
-      required: true,
-      readOnly: false,
-      value: null,
-      placeholder: null,
-      description: null,
-      clickToCopy: false,
-    },
-  ],
+const FIELDS = [
+  {
+    key: 'oAuthRedirectUrl',
+    label: 'OAuth Redirect URL',
+    type: 'string',
+    required: true,
+    readOnly: true,
+    value: '{WEB_APP_URL}/app/google-tasks/connections/add',
+    placeholder: null,
+    description:
+      'When asked to input a redirect URL in Google Cloud, enter the URL above.',
+    clickToCopy: true,
+  },
+  {
+    key: 'clientId',
+    label: 'Client ID',
+    type: 'string',
+    required: true,
+    readOnly: false,
+    value: null,
+    placeholder: null,
+    description: null,
+    clickToCopy: false,
+  },
+  {
+    key: 'clientSecret',
+    label: 'Client Secret',
+    type: 'string',
+    required: true,
+    readOnly: false,
+    value: null,
+    placeholder: null,
+    description: null,
+    clickToCopy: false,
+  },
+];
 
-  generateAuthUrl,
-  verifyCredentials,
-  isStillVerified,
-  refreshToken,
-};
+export { generateAuthUrl, verifyCredentials, isStillVerified, refreshToken, FIELDS };

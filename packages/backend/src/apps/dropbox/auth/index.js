98da8932ffbb0ef610df610f5bf63491dd0cca73
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:32 +0100

    improve

--- packages/backend/src/apps/dropbox/auth/index.js
+++ packages/backend/src/apps/dropbox/auth/index.js
@@ -3,46 +3,43 @@ import verifyCredentials from './verify-credentials.js';
 import isStillVerified from './is-still-verified.js';
 import refreshToken from './refresh-token.js';
 
-export default {
-  fields: [
-    {
-      key: 'oAuthRedirectUrl',
-      label: 'OAuth Redirect URL',
-      type: 'string',
-      required: true,
-      readOnly: true,
-      value: '{WEB_APP_URL}/app/dropbox/connections/add',
-      placeholder: null,
-      description:
-        'When asked to input an OAuth callback or redirect URL in Dropbox OAuth, enter the URL above.',
-      clickToCopy: true,
-    },
-    {
-      key: 'clientId',
-      label: 'App Key',
-      type: 'string',
-      required: true,
-      readOnly: false,
-      value: null,
-      placeholder: null,
-      description: null,
-      clickToCopy: false,
-    },
-    {
-      key: 'clientSecret',
-      label: 'App Secret',
-      type: 'string',
-      required: true,
-      readOnly: false,
-      value: null,
-      placeholder: null,
-      description: null,
-      clickToCopy: false,
-    },
-  ],
+const FIELDS = [
+  {
+    key: 'oAuthRedirectUrl',
+    label: 'OAuth Redirect URL',
+    type: 'string',
+    required: true,
+    readOnly: true,
+    value: '{WEB_APP_URL}/app/dropbox/connections/add',
+    placeholder: null,
+    description:
+      'When asked to input an OAuth callback or redirect URL in Dropbox OAuth, enter the URL above.',
+    clickToCopy: true,
+  },
+  {
+    key: 'clientId',
+    label: 'App Key',
+    type: 'string',
+    required: true,
+    readOnly: false,
+    value: null,
+    placeholder: null,
+    description: null,
+    clickToCopy: false,
+  },
+  {
+    key: 'clientSecret',
+    label: 'App Secret',
+    type: 'string',
+    required: true,
+    readOnly: false,
+    value: null,
+    placeholder: null,
+    description: null,
+    clickToCopy: false,
+  },
+];
 
-  generateAuthUrl,
-  verifyCredentials,
-  isStillVerified,
-  refreshToken,
-};
+export { generateAuthUrl, verifyCredentials, isStillVerified, refreshToken };
+
+export default FIELDS;

66b1f1a61dd4739d012bf225091161ffd7e624b5
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:47 +0100

    improve

--- packages/backend/src/apps/gitlab/common/get-base-url.js
+++ packages/backend/src/apps/gitlab/common/get-base-url.js
@@ -1,13 +1,20 @@
-const getBaseUrl = ($) => {
-  if ($.auth.data.instanceUrl) {
-    return $.auth.data.instanceUrl;
+/**
+ * Returns the base URL for the API, based on the provided `$` object.
+ * The base URL is determined in the following order of priority:
+ * 1. `instanceUrl` property of the `auth` object
+ * 2. `apiBaseUrl` property of the `app` object
+ * 3. `baseUrl` property of the `app` object
+ */
+const getBaseUrl = ({ auth: { data: { instanceUrl } = {} } = {}, app: { apiBaseUrl, baseUrl } = {} }: any = {}) => {
+  if (instanceUrl) {
+    return instanceUrl;
   }
 
-  if ($.app.apiBaseUrl) {
-    return $.app.apiBaseUrl;
+  if (apiBaseUrl) {
+    return apiBaseUrl;
   }
 
-  return $.app.baseUrl;
+  return baseUrl;
 };
 
 export default getBaseUrl;

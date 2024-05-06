52348138b9e3726787eb6d7cb33b42193cf86b93
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:56 +0100

    improve

--- packages/backend/src/apps/google-tasks/auth/refresh-token.js
+++ packages/backend/src/apps/google-tasks/auth/refresh-token.js
@@ -9,17 +9,29 @@ const refreshToken = async ($) => {
     refresh_token: $.auth.data.refreshToken,
   });
 
-  const { data } = await $.http.post(
-    'https://oauth2.googleapis.com/token',
-    params.toString()
-  );
+  try {
+    const response = await $.http.post(
+      'https://oauth2.googleapis.com/token',
+      params
+    );
 
-  await $.auth.set({
-    accessToken: data.access_token,
-    expiresIn: data.expires_in,
-    scope: authScope.join(' '),
-    tokenType: data.token_type,
-  });
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    await $.auth.set({
+      accessToken: data.access_token,
+      expiresIn: data.expires_in,
+      scope: authScope.join(' '),
+      tokenType: data.token_type,
+    });
+
+  } catch (error) {
+    console.error('An error occurred while refreshing the token:', error);
+    throw error;
+  }
 };
 
 export default refreshToken;

9b84b3f6e1b6018e5d75b6a4ee7f5a50dfc96ef7
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:28 +0100

    improve

--- packages/backend/src/apps/reddit/auth/generate-auth-url.js
+++ packages/backend/src/apps/reddit/auth/generate-auth-url.js
@@ -3,23 +3,29 @@ import authScope from '../common/auth-scope.js';
 
 export default async function generateAuthUrl($) {
   const oauthRedirectUrlField = $.app.auth.fields.find(
-    (field) => field.key == 'oAuthRedirectUrl'
+    (field) => field.key === 'oAuthRedirectUrl'
   );
+
+  if (!oauthRedirectUrlField) {
+    throw new Error('Missing oAuthRedirectUrl field');
+  }
+
   const redirectUri = oauthRedirectUrlField.value;
   const state = Math.random().toString();
   const searchParams = new URLSearchParams({
     client_id: $.auth.data.clientId,
     response_type: 'code',
-    redirect_uri: redirectUri,
+    redirect_uri,
     duration: 'permanent',
     scope: authScope.join(' '),
     state,
   });
 
-  const url = `https://www.reddit.com/api/v1/authorize?${searchParams.toString()}`;
+  const authUrl = new URL('https://www.reddit.com/api/v1/authorize');
+  authUrl.search = searchParams.toString();
 
   await $.auth.set({
-    url,
+    url: authUrl.toString(),
     originalState: state,
   });
 }

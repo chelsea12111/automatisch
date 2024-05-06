7ea0597ed374023e39b8988263ff3b06cf118646
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:22 +0100

    improve

--- packages/backend/src/apps/invoice-ninja/actions/index.js
+++ packages/backend/src/apps/invoice-ninja/actions/index.js
@@ -3,4 +3,16 @@ import createInvoice from './create-invoice/index.js';
 import createPayment from './create-payment/index.js';
 import createProduct from './create-product/index.js';
 
-export default [createClient, createInvoice, createPayment, createProduct];
+export {
+  createClient,
+  createInvoice,
+  createPayment,
+  createProduct,
+};
+
+
+import * as factories from './factories.js';
+
+const client = factories.createClient();
+const invoice = factories.createInvoice();
+// ...

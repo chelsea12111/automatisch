6c90f780a38704469d712275b891e12a5c4679ff
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:58 +0100

    improve

--- packages/backend/src/apps/flickr/auth/is-still-verified.js
+++ packages/backend/src/apps/flickr/auth/is-still-verified.js
@@ -1,11 +1,20 @@
-const isStillVerified = async ($) => {
+import axios from 'axios';
+
+const isStillVerified = async (apiKey) => {
   const params = {
     method: 'flickr.test.login',
     format: 'json',
     nojsoncallback: 1,
+    api_key: apiKey,
   };
-  const response = await $.http.get('/rest', { params });
-  return !!response.data.user.id;
+
+  try {
+    const response = await axios.get('/rest', { params });
+    return response.data.user && response.data.user.id !== undefined;
+  } catch (error) {
+    console.error(`Error in isStillVerified: ${error}`);
+    return false;
+  }
 };
 
 export default isStillVerified;

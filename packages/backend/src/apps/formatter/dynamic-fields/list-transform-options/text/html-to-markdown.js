fcbbdf0e50447e037247c2a36b63462e5c1aef39
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:45 +0100

    improve

--- packages/backend/src/apps/formatter/dynamic-fields/list-transform-options/text/html-to-markdown.js
+++ packages/backend/src/apps/formatter/dynamic-fields/list-transform-options/text/html-to-markdown.js
@@ -1,12 +1,14 @@
-const htmlToMarkdown = [
+/**
+ * A constant array of objects defining the schema for the `htmlToMarkdown` function.
+ * Each object represents a single schema field with its properties.
+ * @constant
+ * @type {Array<object>}
+ */
+const htmlToMarkdownSchema = [
   {
-    label: 'Input',
+    label: 'Input HTML',
     key: 'input',
     type: 'string',
     required: true,
-    description: 'HTML that will be converted to Markdown.',
-    variables: true,
-  },
-];
-
-export default htmlToMarkdown;
+    description: 'The HTML that will be converted to Markdown.',
+    //

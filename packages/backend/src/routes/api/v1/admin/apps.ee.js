16082b07b21ebbe16263ecba34b1faaae24238d7
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:32 +0100

    improve

--- packages/backend/src/routes/api/v1/admin/apps.ee.js
+++ packages/backend/src/routes/api/v1/admin/apps.ee.js
@@ -1,26 +1,23 @@
 import { Router } from 'express';
 import asyncHandler from 'express-async-handler';
-import { authenticateUser } from '../../../../helpers/authentication.js';
-import { authorizeAdmin } from '../../../../helpers/authorization.js';
-import { checkIsEnterprise } from '../../../../helpers/check-is-enterprise.js';
+import { authenticateUser, authorizeAdmin, checkIsEnterprise } from '../../../../helpers/authentication.js';
 import getAuthClientsAction from '../../../../controllers/api/v1/admin/apps/get-auth-clients.ee.js';
 import getAuthClientAction from '../../../../controllers/api/v1/admin/apps/get-auth-client.ee.js';
 
 const router = Router();
 
+// Middleware function to reuse authentication and authorization checks
+const authCheck = [authenticateUser, authorizeAdmin, checkIsEnterprise];
+
 router.get(
   '/:appKey/auth-clients',
-  authenticateUser,
-  authorizeAdmin,
-  checkIsEnterprise,
+  ...authCheck,
   asyncHandler(getAuthClientsAction)
 );
 
 router.get(
   '/:appKey/auth-clients/:appAuthClientId',
-  authenticateUser,
-  authorizeAdmin,
-  checkIsEnterprise,
+  ...authCheck,
   asyncHandler(getAuthClientAction)
 );
 

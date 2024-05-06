3a4b1a84fffa0bed198db7f777653f26b5a7e6dc
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:37 +0100

    improve

--- packages/web/src/components/FlowContextMenu/index.jsx
+++ packages/web/src/components/FlowContextMenu/index.jsx
@@ -3,99 +3,95 @@ import { useMutation } from '@apollo/client';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import { useQueryClient } from '@tanstack/react-query';
-
-import useEnqueueSnackbar from 'hooks/useEnqueueSnackbar';
-import * as React from 'react';
 import { Link } from 'react-router-dom';
-
 import Can from 'components/Can';
+import { useEnqueueSnackbar } from 'hooks/useEnqueueSnackbar';
 import * as URLS from 'config/urls';
-import { DELETE_FLOW } from 'graphql/mutations/delete-flow';
-import { DUPLICATE_FLOW } from 'graphql/mutations/duplicate-flow';
 import useFormatMessage from 'hooks/useFormatMessage';
+import { DELETE_FLOW, DUPLICATE_FLOW } from 'graphql/mutations/flow';
 
-function ContextMenu(props) {
-  const { flowId, onClose, anchorEl, onDuplicateFlow, onDeleteFlow, appKey } =
-    props;
+function ContextMenu({
+  flowId,
+  onClose,
+  anchorEl,
+  onDuplicateFlow,
+  onDeleteFlow,
+  appKey,
+}) {
   const enqueueSnackbar = useEnqueueSnackbar();
   const formatMessage = useFormatMessage();
   const queryClient = useQueryClient();
   const [duplicateFlow] = useMutation(DUPLICATE_FLOW);
   const [deleteFlow] = useMutation(DELETE_FLOW);
 
-  const onFlowDuplicate = React.useCallback(async () => {
-    await duplicateFlow({
-      variables: { input: { id: flowId } },
-    });
-
-    await queryClient.invalidateQueries({
-      queryKey: ['apps', appKey, 'flows'],
-    });
-    enqueueSnackbar(formatMessage('flow.successfullyDuplicated'), {
-      variant: 'success',
-      SnackbarProps: {
+  const handleFlowDuplicate = async () => {
+    try {
+      await duplicateFlow({ variables: { input: { id: flowId } } });
+      await queryClient.invalidateQueries(['apps', appKey, 'flows']);
+      enqueueSnackbar(formatMessage('flow.successfullyDuplicated'), {
+        variant: 'success',
         'data-test': 'snackbar-duplicate-flow-success',
-      },
-    });
+      });
+      onDuplicateFlow?.();
+      onClose();
+    } catch (error) {
+      console.error('Error duplicating flow:', error);
+    }
+  };
 
-    onDuplicateFlow?.();
-    onClose();
-  }, [flowId, onClose, duplicateFlow, queryClient, onDuplicateFlow]);
-
-  const onFlowDelete = React.useCallback(async () => {
-    await deleteFlow({
-      variables: { input: { id: flowId } },
-      update: (cache) => {
-        const flowCacheId = cache.identify({
-          __typename: 'Flow',
-          id: flowId,
-        });
-        cache.evict({
-          id: flowCacheId,
-        });
-      },
-    });
-
-    await queryClient.invalidateQueries({
-      queryKey: ['apps', appKey, 'flows'],
-    });
-    enqueueSnackbar(formatMessage('flow.successfullyDeleted'), {
-      variant: 'success',
-    });
-
-    onDeleteFlow?.();
-    onClose();
-  }, [flowId, onClose, deleteFlow, queryClient, onDeleteFlow]);
+  const handleFlowDelete = async () => {
+    try {
+      await deleteFlow({
+        variables: { input: { id: flowId } },
+        update: (cache) => {
+          const flowCacheId = cache.identify({
+            __typename: 'Flow',
+            id: flowId,
+          });
+          cache.evict({ id: flowCacheId });
+        },
+      });
+      await queryClient.invalidateQueries(['apps', appKey, 'flows']);
+      enqueueSnackbar(formatMessage('flow.successfullyDeleted'), {
+        variant: 'success',
+      });
+      onDeleteFlow?.();
+      onClose();
+    } catch (error) {
+      console.error('Error deleting flow:', error);
+    }
+  };
 
   return (
-    <Menu
-      open={true}
-      onClose={onClose}
-      hideBackdrop={false}
-      anchorEl={anchorEl}
-    >
-      <Can I="read" a="Flow" passThrough>
-        {(allowed) => (
-          <MenuItem disabled={!allowed} component={Link} to={URLS.FLOW(flowId)}>
-            {formatMessage('flow.view')}
-          </MenuItem>
-        )}
+    <Menu open={true} onClose={onClose} hideBackdrop={false} anchorEl={anchorEl}>
+      <Can I="read" a="Flow'>
+        {(allowed) =>
+          allowed && (
+            <MenuItem component={Link} to={URLS.FLOW(flowId)}>
+              {formatMessage('flow.view')}
+            </MenuItem>
+          )
+        }
       </Can>
 
-      <Can I="create" a="Flow" passThrough>
-        {(allowed) => (
-          <MenuItem disabled={!allowed} onClick={onFlowDuplicate}>
-            {formatMessage('flow.duplicate')}
-          </MenuItem>
-        )}
+      <Can I="create" a="Flow">
+        {(allowed) =>
+          allowed && (
+            <MenuItem onClick={handleFlowDuplicate}>
+              {formatMessage('flow.duplicate')}
+            </MenuItem>
+          )
+        }
       </Can>
 
-      <Can I="delete" a="Flow" passThrough>
-        {(allowed) => (
-          <MenuItem disabled={!allowed} onClick={onFlowDelete}>
-            {formatMessage('flow.delete')}
-          </MenuItem>
-        )}
+      <Can I="delete" a="Flow">
+        {(allowed) =>
+          allowed && (
+            <MenuItem onClick={handleFlowDelete}>
+              {formatMessage('flow.delete')}
+            </MenuItem>
+          )
+        }
       </Can>
     </Menu>
   );

d0903a73bf6cdcfc83519b227d4380978d61b751
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:11 +0100

    improve

--- packages/backend/src/db/migrations/20230623115503_remove_role_column_in_users.js
+++ packages/backend/src/db/migrations/20230623115503_remove_role_column_in_users.js
@@ -1,11 +1,11 @@
 export async function up(knex) {
-  await knex.schema.table('users', async (table) => {
+  return knex.schema.table('users', (table) => {
     table.dropColumn('role');
   });
 }
 
 export async function down(knex) {
-  return await knex.schema.table('users', (table) => {
-    table.string('role').defaultTo('user');
+  return knex.schema.table('users', (table) => {
+    table.specificType('role', 'text').defaultTo('user');
   });
 }

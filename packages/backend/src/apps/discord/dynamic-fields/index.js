4cf99f52c56b8e8582f97db3de6167484082f4e4
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:36 +0100

    improve

--- packages/backend/src/apps/discord/dynamic-fields/index.js
+++ packages/backend/src/apps/discord/dynamic-fields/index.js
@@ -1,3 +1,11 @@
+// import the `listExternalScheduledEventFields` module
 import listExternalScheduledEventFields from './list-external-scheduled-event-fields/index.js';
 
+// export an array containing the imported module
 export default [listExternalScheduledEventFields];
+
+// or, if you prefer to import and export using the same variable name, you can do the following:
+// import and export a single item as default
+import listExternalScheduledEventFields from './list-external-scheduled-event-fields/index.js';
+
+export default listExternalScheduledEventFields;

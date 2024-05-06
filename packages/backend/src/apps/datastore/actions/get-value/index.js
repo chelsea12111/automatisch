aaaa07a614cb246ca1c5f7275c42e1b4a7f5ea16
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:22 +0100

    improve

--- packages/backend/src/apps/datastore/actions/get-value/index.js
+++ packages/backend/src/apps/datastore/actions/get-value/index.js
@@ -1,27 +1,37 @@
 import defineAction from '../../../../helpers/define-action.js';
 
 export default defineAction({
-  name: 'Get value',
+  name: 'Get Value',
   key: 'getValue',
-  description: 'Get value from the persistent datastore.',
-  arguments: [
+  description: 'Retrieve a value from the persistent datastore using a given key.',
+  document: 'Retrieves the value associated with the provided key from the datastore. If the key does not exist, the action will return `null`.',
+  version: '1.0.0',
+  inputs: [
     {
       label: 'Key',
       key: 'key',
       type: 'string',
+      description: 'The key of the value to retrieve from the datastore.',
       required: true,
-      description: 'The key of your value to get.',
-      variables: true,
+      dynamic: true,
     },
   ],
-
+  outputs: [
+    {
+      label: 'Value',
+      key: 'value',
+      type: 'any',
+      description: 'The value associated with the provided key in the datastore.',
+    },
+  ],
   async run($) {
-    const keyValuePair = await $.datastore.get({
-      key: $.step.parameters.key,
-    });
+    const key = $.inputs.key;
+    const keyValuePair = await $.datastore.get({ key });
 
-    $.setActionItem({
-      raw: keyValuePair,
-    });
+    if (keyValuePair) {
+      $.outputs.value = keyValuePair.value;
+    } else {
+      $.outputs.value = null;
+    }
   },
 });

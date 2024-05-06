8efd448d7d44eb1d235983f60f97ae17060c78aa
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:23 +0100

    improve

--- packages/backend/src/app.js
+++ packages/backend/src/app.js
@@ -1,24 +1,16 @@
-import createError from 'http-errors';
 import express from 'express';
 import cors from 'cors';
-
-import appConfig from './config/app.js';
-import corsOptions from './config/cors-options.js';
 import morgan from './helpers/morgan.js';
 import * as Sentry from './helpers/sentry.ee.js';
 import appAssetsHandler from './helpers/app-assets-handler.js';
-import webUIHandler from './helpers/web-ui-handler.js';
 import errorHandler from './helpers/error-handler.js';
-import './config/orm.js';
-import {
-  createBullBoardHandler,
-  serverAdapter,
-} from './helpers/create-bull-board-handler.js';
-import injectBullBoardHandler from './helpers/inject-bull-board-handler.js';
-import router from './routes/index.js';
+import { orm } from './config/orm.js';
+import { createBullBoardHandler, serverAdapter } from './helpers/create-bull-board-handler.js';
 import configurePassport from './helpers/passport.js';
+import router from './routes/index.js';
 
-createBullBoardHandler(serverAdapter);
+const { requestBodySizeLimit } = await import('./config/app.js');
+const corsOptions = { origin: '*' };
 
 const app = express();
 
@@ -27,34 +19,36 @@ Sentry.init(app);
 Sentry.attachRequestHandler(app);
 Sentry.attachTracingHandler(app);
 
-injectBullBoardHandler(app, serverAdapter);
-
-appAssetsHandler(app);
-
 app.use(morgan);
 
-app.use(
-  express.json({
-    limit: appConfig.requestBodySizeLimit,
-    verify(req, res, buf) {
-      req.rawBody = buf;
-    },
-  })
-);
-app.use(
-  express.urlencoded({
-    extended: true,
-    limit: appConfig.requestBodySizeLimit,
-    verify(req, res, buf) {
-      req.rawBody = buf;
-    },
-  })
-);
+const jsonParser = express.json({
+  limit: requestBodySizeLimit,
+  verify: (req, res, buf) => {
+    req.rawBody = buf;
+  },
+});
+
+const urlencodedParser = express.urlencoded({
+  extended: true,
+  limit: requestBodySizeLimit,
+  verify: (req, res, buf) => {
+    req.rawBody = buf;
+  },
+});
+
+app.use(jsonParser);
+app.use(urlencodedParser);
+
 app.use(cors(corsOptions));
 
 configurePassport(app);
 
 app.use('/', router);
+
+createBullBoardHandler(serverAdapter);
+injectBullBoardHandler(app, serverAdapter);
+
+appAssetsHandler(app);
 
 webUIHandler(app);
 

9f7993e7a83bdb07a60fc19c1f1acef677da44ce
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:43 +0100

    improve

--- packages/backend/src/apps/ghost/triggers/index.js
+++ packages/backend/src/apps/ghost/triggers/index.js
@@ -1,3 +1,7 @@
-import newPostPublished from './new-post-published/index.js';
+// Import the `newPostPublished` function/module from its relative path
+import newPostPublished from './new-post-published.js';
 
-export default [newPostPublished];
+// Export the `newPostPublished` function/module as the default export
+// of this module, using the ES6 syntax for default exports
+export { newPostPublished as default };
+

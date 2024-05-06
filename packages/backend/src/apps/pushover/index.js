dd00607cdaaa13bbac2f31038d5b55b3948fdfe2
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:54 +0100

    improve

--- packages/backend/src/apps/pushover/index.js
+++ packages/backend/src/apps/pushover/index.js
@@ -3,16 +3,20 @@ import auth from './auth/index.js';
 import actions from './actions/index.js';
 import dynamicData from './dynamic-data/index.js';
 
-export default defineApp({
+const appConfig = {
   name: 'Pushover',
   key: 'pushover',
   baseUrl: 'https://pushover.net',
   apiBaseUrl: 'https://api.pushover.net',
-  iconUrl: '{BASE_URL}/apps/pushover/assets/favicon.svg',
-  authDocUrl: '{DOCS_URL}/apps/pushover/connection',
-  primaryColor: '249DF1',
+  iconUrl: `${appConfig.baseUrl}/apps/pushover/assets/favicon.svg`,
+  authDocUrl: `${appConfig.docsUrl}/apps/pushover/connection`,
+  primaryColor: '#249DF1',
   supportsConnections: true,
   auth,
   actions,
   dynamicData,
-});
+};
+
+appConfig.docsUrl = defineApp.docsUrl;
+
+export default appConfig;

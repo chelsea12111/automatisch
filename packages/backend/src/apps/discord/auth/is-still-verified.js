77c859460d43adefed8a0f4f421f0636bb86a1f9
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:58:26 +0100

    improve

--- packages/backend/src/apps/discord/auth/is-still-verified.js
+++ packages/backend/src/apps/discord/auth/is-still-verified.js
@@ -1,9 +1,13 @@
 import getCurrentUser from '../common/get-current-user.js';
 
-const isStillVerified = async ($) => {
-  await getCurrentUser($);
+export default async function isStillVerified($) {
+  try {
+    await getCurrentUser($);
+    return true;
+  } catch (error) {
+    // Handle the error, e.g. by logging it or rejecting the promise
+    console.error(error);
+    throw error;
+  }
+}
 
-  return true;
-};
-
-export default isStillVerified;

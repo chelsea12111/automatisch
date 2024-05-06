bc6236a811fc00b4a69e7e08029c9bbecbf4599a
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:26 +0100

    improve

--- packages/backend/src/controllers/api/v1/apps/get-auth-client.ee.test.js
+++ packages/backend/src/controllers/api/v1/apps/get-auth-client.ee.test.js
@@ -1,50 +1,83 @@
-import { vi, describe, it, expect, beforeEach } from 'vitest';
+import { vi, describe, it, expect, beforeEach, afterEach } from 'vitest';
 import request from 'supertest';
 import Crypto from 'crypto';
 import app from '../../../../app.js';
 import createAuthTokenByUserId from '../../../../helpers/create-auth-token-by-user-id.js';
-import { createUser } from '../../../../../test/factories/user.js';
+import { createUser, cleanupUsers } from '../../../../../test/factories/user.js';
 import getAppAuthClientMock from '../../../../../test/mocks/rest/api/v1/apps/get-auth-client.js';
 import { createAppAuthClient } from '../../../../../test/factories/app-auth-client.js';
 import * as license from '../../../../helpers/license.ee.js';
 
+let currentUser, currentAppAuthClient, token, hasValidLicenseMock;
+
+const teardown = () => {
+  hasValidLicenseMock.mockReset();
+};
+
+const afterEachTest = async () => {
+  await cleanupUsers();
+  currentUser = null;
+  currentAppAuthClient = null;
+  token = null;
+};
+
 describe('GET /api/v1/apps/:appKey/auth-clients/:appAuthClientId', () => {
-  let currentUser, currentAppAuthClient, token;
+  beforeAll(() => {
+    hasValidLicenseMock = vi.spyOn(license, 'hasValidLicense').mockResolvedValue(true);
+  });
 
-  beforeEach(async () => {
-    vi.spyOn(license, 'hasValidLicense').mockResolvedValue(true);
+  afterEach(() => {
+    teardown();
+    afterEachTest();
+  });
 
-    currentUser = await createUser();
-    currentAppAuthClient = await createAppAuthClient({
-      appKey: 'deepl',
+  describe('when license is valid', () => {
+    beforeEach(async () => {
+      currentUser = await createUser();
+      currentAppAuthClient = await createAppAuthClient({
+        appKey: 'deepl',
+      });
+
+      token = await createAuthTokenByUserId(currentUser.id);
     });
 
-    token = await createAuthTokenByUserId(currentUser.id);
+    it('should return specified app auth client', async () => {
+      const response = await request(app)
+        .get(`/api/v1/apps/deepl/auth-clients/${currentAppAuthClient.id}`)
+        .set('Authorization', token)
+        .expect(200);
+
+      const expectedPayload = getAppAuthClientMock(currentAppAuthClient);
+      expect(response.body).toEqual(expectedPayload);
+    });
+
+    it('should return not found response for not existing app auth client ID', async () => {
+      const notExistingAppAuthClientUUID = Crypto.randomUUID();
+
+      await request(app)
+        .get(`/api/v1/apps/deepl/auth-clients/${notExistingAppAuthClientUUID}`)
+        .set('Authorization', token)
+        .expect(404);
+    });
+
+    it('should return bad request response for invalid UUID', async () => {
+      await request(app)
+        .get('/api/v1/apps/deepl/auth-clients/invalidAppAuthClientUUID')
+        .set('Authorization', token)
+        .expect(400);
+    });
   });
 
-  it('should return specified app auth client', async () => {
-    const response = await request(app)
-      .get(`/api/v1/apps/deepl/auth-clients/${currentAppAuthClient.id}`)
-      .set('Authorization', token)
-      .expect(200);
+  describe('when license is not valid', () => {
+    beforeEach(() => {
+      hasValidLicenseMock.mockResolvedValue(false);
+    });
 
-    const expectedPayload = getAppAuthClientMock(currentAppAuthClient);
-    expect(response.body).toEqual(expectedPayload);
-  });
-
-  it('should return not found response for not existing app auth client ID', async () => {
-    const notExistingAppAuthClientUUID = Crypto.randomUUID();
-
-    await request(app)
-      .get(`/api/v1/apps/deepl/auth-clients/${notExistingAppAuthClientUUID}`)
-      .set('Authorization', token)
-      .expect(404);
-  });
-
-  it('should return bad request response for invalid UUID', async () => {
-    await request(app)
-      .get('/api/v1/apps/deepl/auth-clients/invalidAppAuthClientUUID')
-      .set('Authorization', token)
-      .expect(400);
+    it('should return unauthorized response', async () => {
+      await request(app)
+        .get('/api/v1/apps/deepl/auth-clients/validAppAuthClientUUID')
+        .set('Authorization', token)
+        .expect(401);
+    });
   });
 });

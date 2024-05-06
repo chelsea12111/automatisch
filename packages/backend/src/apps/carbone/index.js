1a8ba08f1431f58e2354639f0ebe28d14c9b9426
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:26 +0100

    improve

--- packages/backend/src/apps/carbone/index.js
+++ packages/backend/src/apps/carbone/index.js
@@ -1,18 +1,29 @@
 import defineApp from '../../helpers/define-app.js';
 import addAuthHeader from './common/add-auth-header.js';
-import auth from './auth/index.js';
-import actions from './actions/index.js';
+import { auth as authActions, actions as appActions } from './actions/index.js';
+import authModule from './auth/index.js';
+
+const { getAuthHeader } = authModule;
 
 export default defineApp({
   name: 'Carbone',
   key: 'carbone',
-  iconUrl: '{BASE_URL}/apps/carbone/assets/favicon.svg',
-  authDocUrl: '{DOCS_URL}/apps/carbone/connection',
+  iconUrl: `${process.env.BASE_URL}/apps/carbone/assets/favicon.svg`,
+  authDocUrl: `${process.env.DOCS_URL}/apps/carbone/connection`,
   supportsConnections: true,
   baseUrl: 'https://carbone.io',
   apiBaseUrl: 'https://api.carbone.io',
   primaryColor: '6f42c1',
-  beforeRequest: [addAuthHeader],
-  auth,
-  actions,
+  beforeRequest: [request => {
+    const authHeader = getAuthHeader();
+    if (authHeader) {
+      request.headers.set('Authorization', authHeader);
+    }
+    return request;
+  }],
+  auth: authModule,
+  actions: {
+    ...appActions,
+    auth: authActions,
+  },
 });

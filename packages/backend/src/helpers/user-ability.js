9f623f1e515a91ec7b17b48f00cc7065d69c0fde
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 23:00:36 +0100

    improve

--- packages/backend/src/helpers/user-ability.js
+++ packages/backend/src/helpers/user-ability.js
@@ -1,23 +1,14 @@
-import {
-  PureAbility,
-  fieldPatternMatcher,
-  mongoQueryMatcher,
-} from '@casl/ability';
+import { PureAbility, fieldPatternMatcher, mongoQueryMatcher } from '@casl/ability';
 
-// Must be kept in sync with `packages/web/src/helpers/userAbility.ts`!
 export default function userAbility(user) {
-  const permissions = user?.permissions;
-  const role = user?.role;
+  const permissions = user?.permissions || [];
+  const role = user?.role || 'guest'; // Set a default value for role
 
-  // We're not using mongo, but our fields, conditions match
   const options = {
     conditionsMatcher: mongoQueryMatcher,
     fieldMatcher: fieldPatternMatcher,
   };
 
-  if (!role || !permissions) {
-    return new PureAbility([], options);
-  }
-
   return new PureAbility(permissions, options);
 }
+

8a4b0a3b7a8cac2ed4ab5f9cf929c0ccf4f1d239
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:46 +0100

    improve

--- packages/backend/src/apps/placetel/auth/index.js
+++ packages/backend/src/apps/placetel/auth/index.js
@@ -1,21 +1,27 @@
 import verifyCredentials from './verify-credentials.js';
 import isStillVerified from './is-still-verified.js';
 
-export default {
-  fields: [
-    {
-      key: 'apiToken',
-      label: 'API Token',
-      type: 'string',
-      required: true,
-      readOnly: false,
-      value: null,
-      placeholder: null,
-      description: 'Placetel API Token of your account.',
-      clickToCopy: false,
-    },
-  ],
+const fields = [
+  {
+    key: 'apiToken',
+    label: 'API Token',
+    type: 'string',
+    required: true,
+    readOnly: false,
+    value: null,
+    placeholder: null,
+    description: 'Placetel API Token of your account.',
+    clickToCopy: false,
+  },
+];
 
+const config = {
   verifyCredentials,
   isStillVerified,
+};
+
+// Export an object with both fields and config properties
+export default {
+  fields,
+  config,
 };

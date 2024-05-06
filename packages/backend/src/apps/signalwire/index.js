c443f1a60e64138483b22f296d418a446d6649f5
chelsea12111 <143886687+chelsea12111@users.noreply.github.com>
2024-05-06 22:59:52 +0100

    improve

--- packages/backend/src/apps/signalwire/index.js
+++ packages/backend/src/apps/signalwire/index.js
@@ -1,9 +1,14 @@
 import defineApp from '../../helpers/define-app.js';
 import addAuthHeader from './common/add-auth-header.js';
-import auth from './auth/index.js';
-import triggers from './triggers/index.js';
-import actions from './actions/index.js';
-import dynamicData from './dynamic-data/index.js';
+import authModule from './auth/index.js';
+import triggersModule from './triggers/index.js';
+import actionsModule from './actions/index.js';
+import dynamicDataModule from './dynamic-data/index.js';
+
+const { auth } = authModule;
+const { triggers } = triggersModule;
+const { actions } = actionsModule;
+const { dynamicData } = dynamicDataModule;
 
 export default defineApp({
   name: 'SignalWire',
